e3e8d0cae428aba5f2563916495c1144906cbd01
Dominic Clifton <me@dominicclifton.name>
2016-03-31 22:46:01 +0200

    remove unneeded dependencies from ltm.c and smartport.c now that it no-longer requires config_master.h

--- src/main/telemetry/ltm.c
+++ src/main/telemetry/ltm.c
@@ -39,30 +39,20 @@
 
 #include "common/maths.h"
 #include "common/axis.h"
-#include "common/color.h"
 
 #include "config/parameter_group.h"
 
 #include "drivers/system.h"
 #include "drivers/sensor.h"
 #include "drivers/accgyro.h"
-#include "drivers/gpio.h"
-#include "drivers/timer.h"
 #include "drivers/serial.h"
-#include "drivers/pwm_rx.h"
 
 #include "sensors/sensors.h"
 #include "sensors/acceleration.h"
-#include "sensors/gyro.h"
-#include "sensors/barometer.h"
-#include "sensors/boardalignment.h"
 #include "sensors/battery.h"
 
 #include "io/serial.h"
-#include "io/rc_controls.h"
-#include "io/gimbal.h"
 #include "io/gps.h"
-#include "io/ledstrip.h"
 
 #include "rx/rx.h"
 
@@ -78,7 +68,6 @@
 #include "config/config.h"
 #include "config/runtime_config.h"
 #include "config/config_profile.h"
-#include "config/config_master.h"
 
 #define TELEMETRY_LTM_INITIAL_PORT_MODE MODE_TX
 #define LTM_CYCLETIME   100
