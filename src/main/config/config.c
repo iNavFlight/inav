betaflight/betaflight

extract imu configuration from master_t.

src/main/config/config.c
@@ -340,9 +340,13 @@ STATIC_UNIT_TESTED void resetConf(void)
 
     featureSet(FEATURE_FAILSAFE);
 
-    // global settings
-    masterConfig.small_angle = 25;
-    masterConfig.dcm_kp = 2500;                // 1.0 * 10000
+    // imu settings
+    imuConfig.small_angle = 25;
+    imuConfig.dcm_kp = 2500;                // 1.0 * 10000
+    imuConfig.looptime = 2000;
+    imuConfig.gyroSync = 1;
+    imuConfig.gyroSyncDenominator = 1;
+
     gyroConfig.gyro_lpf = 1;                 // supported by all gyro drivers now. In case of ST gyro, will default to 32Hz instead
     gyroConfig.soft_gyro_lpf_hz = 60;        // Software based lpf filter for gyro
 
@@ -396,10 +400,6 @@ STATIC_UNIT_TESTED void resetConf(void)
 #endif
 
     resetSerialConfig(&serialConfig);
-
-    masterConfig.looptime = 2000;
-    masterConfig.gyroSync = 1;
-    masterConfig.gyroSyncDenominator = 1;
 
     systemConfig.i2c_highspeed = 1;
 
@@ -648,11 +648,11 @@ void activateConfig(void)
         &masterConfig.rxConfig
     );
 
-    imuRuntimeConfig.dcm_kp = masterConfig.dcm_kp / 10000.0f;
-    imuRuntimeConfig.dcm_ki = masterConfig.dcm_ki / 10000.0f;
+    imuRuntimeConfig.dcm_kp = imuConfig.dcm_kp / 10000.0f;
+    imuRuntimeConfig.dcm_ki = imuConfig.dcm_ki / 10000.0f;
     imuRuntimeConfig.acc_cut_hz = currentProfile->acc_cut_hz;
     imuRuntimeConfig.acc_unarmedcal = currentProfile->acc_unarmedcal;
-    imuRuntimeConfig.small_angle = masterConfig.small_angle;
+    imuRuntimeConfig.small_angle = imuConfig.small_angle;
 
     imuConfigure(
         &imuRuntimeConfig,

src/main/io/serial_cli.c
@@ -491,11 +491,11 @@ typedef struct {
 } __attribute__((packed)) clivalue_t;
 
 const clivalue_t valueTable[] = {
-    { "looptime",                   VAR_UINT16 | MASTER_VALUE,  &masterConfig.looptime, .config.minmax = {0, 9000} },
+    { "looptime",                   VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = {0, 9000} , PG_IMU_CONFIG, offsetof(imuConfig_t, looptime)},
     { "emf_avoidance",              VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_SYSTEM_CONFIG, offsetof(systemConfig_t, emf_avoidance)},
     { "i2c_highspeed",              VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_SYSTEM_CONFIG, offsetof(systemConfig_t, i2c_highspeed)},
-    { "gyro_sync",                  VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP,  &masterConfig.gyroSync, .config.lookup = { TABLE_OFF_ON } },
-    { "gyro_sync_denom",            VAR_UINT8  | MASTER_VALUE,  &masterConfig.gyroSyncDenominator, .config.minmax = { 1,  32 } },
+    { "gyro_sync",                  VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_IMU_CONFIG, offsetof(imuConfig_t, gyroSync)},
+    { "gyro_sync_denom",            VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 1,  32 } , PG_IMU_CONFIG, offsetof(imuConfig_t, gyroSyncDenominator)},
 
     { "mid_rc",                     VAR_UINT16 | MASTER_VALUE,  &masterConfig.rxConfig.midrc, .config.minmax = { 1200,  1700 } },
     { "min_check",                  VAR_UINT16 | MASTER_VALUE,  &masterConfig.rxConfig.mincheck, .config.minmax = { PWM_RANGE_ZERO,  PWM_RANGE_MAX } },
@@ -526,7 +526,7 @@ const clivalue_t valueTable[] = {
     { "auto_disarm_delay",          VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  60 } , PG_ARMING_CONFIG, offsetof(armingConfig_t, auto_disarm_delay)},
     { "max_arm_angle",              VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  180 } , PG_ARMING_CONFIG, offsetof(armingConfig_t, max_arm_angle)},
 
-    { "small_angle",                VAR_UINT8  | MASTER_VALUE,  &masterConfig.small_angle, .config.minmax = { 0,  180 } },
+    { "small_angle",                VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  180 } , PG_IMU_CONFIG, offsetof(imuConfig_t, small_angle)},
 
     { "fixedwing_althold_dir",      VAR_INT8   | MASTER_VALUE,  &masterConfig.airplaneConfig.fixedwing_althold_dir, .config.minmax = { -1,  1 } },
 
@@ -596,8 +596,8 @@ const clivalue_t valueTable[] = {
     { "gyro_lpf",                   VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_GYRO_LPF } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, gyro_lpf)},
     { "gyro_soft_lpf",              VAR_FLOAT  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  500 } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, soft_gyro_lpf_hz)},
     { "moron_threshold",            VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  128 } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, gyroMovementCalibrationThreshold)},
-    { "imu_dcm_kp",                 VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_kp, .config.minmax = { 0,  20000 } },
-    { "imu_dcm_ki",                 VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_ki, .config.minmax = { 0,  20000 } },
+    { "imu_dcm_kp",                 VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  20000 } , PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_kp)},
+    { "imu_dcm_ki",                 VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  20000 } , PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_ki)},
 
     { "alt_hold_deadband",          VAR_UINT8  | PROFILE_VALUE, 0,  .config.minmax = { 1,  250 } , PG_PROFILE, offsetof(profile_t, rcControlsConfig.alt_hold_deadband)},
     { "alt_hold_fast_change",       VAR_UINT8  | PROFILE_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_PROFILE, offsetof(profile_t, rcControlsConfig.alt_hold_fast_change)},

iNavFlight/inav

src/main/config/config.c
@@ -407,25 +407,22 @@ STATIC_UNIT_TESTED void resetConf(void)
 
     featureSet(FEATURE_FAILSAFE);
 
-    // global settings
-    masterConfig.dcm_kp_acc = 2500;             // 0.25 * 10000
-    masterConfig.dcm_ki_acc = 50;               // 0.005 * 10000
-    masterConfig.dcm_kp_mag = 10000;            // 1.00 * 10000
-    masterConfig.dcm_ki_mag = 0;                // 0.00 * 10000
+    // imu settings
+    imuConfig.small_angle = 25;
+    imuConfig.dcm_kp_acc = 2500;             // 0.25 * 10000
+    imuConfig.dcm_ki_acc = 50;               // 0.005 * 10000
+    imuConfig.dcm_kp_mag = 10000;            // 1.00 * 10000
+    imuConfig.dcm_ki_mag = 0;                // 0.00 * 10000
+
+    imuConfig.looptime = 2000;
+    imuConfig.gyroSync = 1;
+    imuConfig.gyroSyncDenominator = 2;
 
     resetGyroConfig();
 
     resetAccelerometerConfig();
 
     resetSensorAlignment(&sensorAlignmentConfig);
-
-    masterConfig.small_angle = 25;
-
-    /*
-    masterConfig.acc_hardware = ACC_DEFAULT;     // default/autodetect
-    masterConfig.mag_hardware = MAG_DEFAULT;     // default/autodetect
-    masterConfig.baro_hardware = BARO_DEFAULT;   // default/autodetect
-    */
 
     resetBatteryConfig(&batteryConfig);
 
@@ -481,10 +478,6 @@ STATIC_UNIT_TESTED void resetConf(void)
 #endif
 
     resetSerialConfig(&serialConfig);
-
-    masterConfig.looptime = 2000;
-    masterConfig.gyroSync = 1;
-    masterConfig.gyroSyncDenominator = 2;
 
     systemConfig.i2c_highspeed = 1;
 
@@ -712,11 +705,11 @@ void activateConfig(void)
         &masterConfig.rxConfig
     );
 
-    imuRuntimeConfig.dcm_kp_acc = masterConfig.dcm_kp_acc / 10000.0f;
-    imuRuntimeConfig.dcm_ki_acc = masterConfig.dcm_ki_acc / 10000.0f;
-    imuRuntimeConfig.dcm_kp_mag = masterConfig.dcm_kp_mag / 10000.0f;
-    imuRuntimeConfig.dcm_ki_mag = masterConfig.dcm_ki_mag / 10000.0f;
-    imuRuntimeConfig.small_angle = masterConfig.small_angle;
+    imuRuntimeConfig.dcm_kp_acc = imuConfig.dcm_kp_acc / 10000.0f;
+    imuRuntimeConfig.dcm_ki_acc = imuConfig.dcm_ki_acc / 10000.0f;
+    imuRuntimeConfig.dcm_kp_mag = imuConfig.dcm_kp_mag / 10000.0f;
+    imuRuntimeConfig.dcm_ki_mag = imuConfig.dcm_ki_mag / 10000.0f;
+    imuRuntimeConfig.small_angle = imuConfig.small_angle;
 
     imuConfigure(&imuRuntimeConfig);
 
@@ -776,13 +769,13 @@ void validateAndFixConfig(void)
 
 #ifdef STM32F10X
     // avoid overloading the CPU on F1 targets when using gyro sync and GPS.
-    if (masterConfig.gyroSync && masterConfig.gyroSyncDenominator < 2 && featureConfigured(FEATURE_GPS)) {
-        masterConfig.gyroSyncDenominator = 2;
+    if (imuConfig.gyroSync && imuConfig.gyroSyncDenominator < 2 && featureConfigured(FEATURE_GPS)) {
+        imuConfig.gyroSyncDenominator = 2;
     }
 
     // avoid overloading the CPU when looptime < 2000 and GPS
-    if (masterConfig.looptime && featureConfigured(FEATURE_GPS)) {
-        masterConfig.looptime = 2000;
+    if (imuConfig.looptime && featureConfigured(FEATURE_GPS)) {
+        imuConfig.looptime = 2000;
     }
 #endif
 

src/main/io/serial_cli.c
@@ -511,12 +511,11 @@ typedef struct {
 } __attribute__((packed)) clivalue_t;
 
 const clivalue_t valueTable[] = {
-    { "looptime",                   VAR_UINT16 | MASTER_VALUE,  &masterConfig.looptime, .config.minmax = {0, 9000} },
+    { "looptime",                   VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = {0, 9000} , PG_IMU_CONFIG, offsetof(imuConfig_t, looptime)},
     { "emf_avoidance",              VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_SYSTEM_CONFIG, offsetof(systemConfig_t, emf_avoidance)},
     { "i2c_highspeed",              VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_SYSTEM_CONFIG, offsetof(systemConfig_t, i2c_highspeed)},
-
-    { "gyro_sync",                  VAR_UINT8  | MASTER_VALUE | MODE_LOOKUP,  &masterConfig.gyroSync, .config.lookup = { TABLE_OFF_ON } },
-    { "gyro_sync_denom",            VAR_UINT8  | MASTER_VALUE,  &masterConfig.gyroSyncDenominator, .config.minmax = { 1,  32 } },
+    { "gyro_sync",                  VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_OFF_ON } , PG_IMU_CONFIG, offsetof(imuConfig_t, gyroSync)},
+    { "gyro_sync_denom",            VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 1,  32 } , PG_IMU_CONFIG, offsetof(imuConfig_t, gyroSyncDenominator)},
 
     { "mid_rc",                     VAR_UINT16 | MASTER_VALUE,  &masterConfig.rxConfig.midrc, .config.minmax = { 1200,  1700 }, 0 },
     { "min_check",                  VAR_UINT16 | MASTER_VALUE,  &masterConfig.rxConfig.mincheck, .config.minmax = { PWM_RANGE_ZERO,  PWM_RANGE_MAX }, 0 },
@@ -545,7 +544,7 @@ const clivalue_t valueTable[] = {
     { "auto_disarm_delay",          VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  60 } , PG_ARMING_CONFIG, offsetof(armingConfig_t, auto_disarm_delay)},
     { "max_arm_angle",              VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  180 } , PG_ARMING_CONFIG, offsetof(armingConfig_t, max_arm_angle)},
 
-    { "small_angle",                VAR_UINT8  | MASTER_VALUE,  &masterConfig.small_angle, .config.minmax = { 0,  180 } },
+    { "small_angle",                VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  180 } , PG_IMU_CONFIG, offsetof(imuConfig_t, small_angle)},
 
     { "reboot_character",           VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 48,  126 } , PG_SERIAL_CONFIG, offsetof(serialConfig_t, reboot_character)},
 
@@ -665,10 +664,10 @@ const clivalue_t valueTable[] = {
     { "gyro_lpf",                   VAR_UINT8  | MIGRATED_MASTER_VALUE | MODE_LOOKUP, 0,  .config.lookup = { TABLE_GYRO_LPF } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, gyro_lpf)},
     { "moron_threshold",            VAR_UINT8  | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  128 } , PG_GYRO_CONFIG, offsetof(gyroConfig_t, gyroMovementCalibrationThreshold)},
 
-    { "imu_dcm_kp",                 VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_kp_acc, .config.minmax = { 0,  65535 }, 0 },
-    { "imu_dcm_ki",                 VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_ki_acc, .config.minmax = { 0,  65535 }, 0 },
-    { "imu_dcm_kp_mag",             VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_kp_mag, .config.minmax = { 0,  65535 }, 0 },
-    { "imu_dcm_ki_mag",             VAR_UINT16 | MASTER_VALUE,  &masterConfig.dcm_ki_mag, .config.minmax = { 0,  65535 }, 0 },
+    { "imu_dcm_kp",                 VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  20000 }, PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_kp_acc)},
+    { "imu_dcm_ki",                 VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  20000 }, PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_ki_acc)},
+    { "imu_dcm_kp_mag",             VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  65535 }, PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_kp_mag)},
+    { "imu_dcm_ki_mag",             VAR_UINT16 | MIGRATED_MASTER_VALUE, 0,  .config.minmax = { 0,  65535 }, PG_IMU_CONFIG, offsetof(imuConfig_t, dcm_ki_mag)},
 
     { "deadband",                   VAR_UINT8  | PROFILE_VALUE, 0,  .config.minmax = { 0,  32 } , PG_PROFILE, offsetof(profile_t, rcControlsConfig.deadband)},
     { "yaw_deadband",               VAR_UINT8  | PROFILE_VALUE, 0,  .config.minmax = { 0,  100 } , PG_PROFILE, offsetof(profile_t, rcControlsConfig.yaw_deadband)},