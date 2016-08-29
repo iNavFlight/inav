23ec688ab630b8b6922d3b4206742fbb39e96994
Martin Budden <mjbudden@gmail.com>
2016-08-29 10:05:45 +0100

    Fixed RMDO target

--- src/main/target/RMDO/target.mk
+++ src/main/target/RMDO/target.mk
@@ -8,6 +8,7 @@ TARGET_SRC = \
             drivers/barometer_bmp085.c \
             drivers/barometer_bmp280.c \
             drivers/barometer_ms5611.c \
+            drivers/compass_ak8963.c \
             drivers/compass_ak8975.c \
             drivers/compass_hmc5883l.c \
             drivers/compass_mag3110.c \
