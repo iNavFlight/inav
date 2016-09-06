cleanflight/cleanflight

Add ibus telemetry support

Makefile
@@ -277,6 +277,7 @@ HIGHEND_SRC = \
 		   telemetry/hott.c \
 		   telemetry/smartport.c \
 		   telemetry/ltm.c \
+		   telemetry/ibus.c \
 		   sensors/sonar.c \
 		   sensors/barometer.c \
 		   blackbox/blackbox.c \

iNavFlight/inav

Makefile
@@ -463,7 +463,8 @@ HIGHEND_SRC = \
             telemetry/hott.c \
             telemetry/smartport.c \
             telemetry/mavlink.c \
-            telemetry/ltm.c
+            telemetry/ltm.c \
+			telemetry/ibus.c
 
 ifeq ($(TARGET),$(filter $(TARGET),$(F4_TARGETS)))
 VCP_SRC = \