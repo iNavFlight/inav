1848e9b317b08916aba07730ea47a50ec33ed275
Sami Korhonen <samik.korhonen@gmail.com>
2016-10-17 19:40:29 +0300

    F7 target

--- src/main/target/ANYFCF7/target.mk
+++ src/main/target/ANYFCF7/target.mk
@@ -4,6 +4,8 @@ FEATURES       += SDCARD VCP
 TARGET_SRC = \
             drivers/accgyro_spi_mpu6000.c \
             drivers/barometer_ms5611.c \
+            drivers/compass_hmc5883l.c \
             drivers/light_ws2811strip.c \
-            drivers/light_ws2811strip_hal.c
+            drivers/light_ws2811strip_hal.c \
+            drivers/pitotmeter_ms4525.c
 
