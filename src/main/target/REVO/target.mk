976573d283e401dbe05250b320b2dfda68c33d26
blckmn <j@blck.mn>
2016-11-03 15:35:37 +1100

    Adding Revolt target
    # Conflicts:
    #	src/main/drivers/accgyro_mpu6500.h
    #	src/main/drivers/accgyro_spi_mpu6500.c
    #	src/main/target/REVO/target.h
    #	src/main/target/REVO/target.mk

--- src/main/target/REVO/target.mk
+++ src/main/target/REVO/target.mk
@@ -3,6 +3,8 @@ FEATURES       += VCP ONBOARDFLASH
 
 TARGET_SRC = \
             drivers/accgyro_spi_mpu6000.c \
+            drivers/accgyro_mpu6500.c \
+            drivers/accgyro_spi_mpu6500.c \
             drivers/barometer_bmp085.c \
             drivers/barometer_bmp280.c \
             drivers/barometer_ms56xx.c \
