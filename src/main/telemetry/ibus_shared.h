c21dc4202cdea535c3e6f68702070646a37d0f9b
hali9 <hali9@wp.pl>
2017-02-08 18:32:45 +0100

    Update ibus_shared.h

--- src/main/telemetry/ibus_shared.h
+++ src/main/telemetry/ibus_shared.h
@@ -20,6 +20,14 @@
 #include "io/serial.h"
 
 #define IBUS_CHECKSUM_SIZE (2)
+#define IBUS_TASK_PERIOD_US (500)
+#define IBUS_UART_MODE     (MODE_RXTX)
+#define IBUS_BAUDRATE      (115200)
+#define IBUS_CYCLE_TIME_MS (8)
+#define IBUS_MIN_LEN       (2 + IBUS_CHECKSUM_SIZE)
+#define IBUS_MAX_TX_LEN    (6)
+#define IBUS_MAX_RX_LEN    (4)
+#define IBUS_RX_BUF_LEN    (IBUS_MAX_RX_LEN)
 
 #if defined(TELEMETRY) && defined(TELEMETRY_IBUS)
 
