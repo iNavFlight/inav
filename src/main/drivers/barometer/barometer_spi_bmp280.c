7a708dad5185b9f013ee0e51e443d4be1d9c8e07
giacomo <giacomo892@users.noreply.github.com>
2017-05-07 18:40:12 +0200

    Fix SPI targets

--- src/main/drivers/barometer/barometer_spi_bmp280.c
+++ src/main/drivers/barometer/barometer_spi_bmp280.c
@@ -30,8 +30,8 @@
 #define DISABLE_BMP280       IOHi(bmp280CsPin)
 #define ENABLE_BMP280        IOLo(bmp280CsPin)
 
-extern int32_t bmp280_up;
-extern int32_t bmp280_ut;
+extern static int32_t bmp280_up;
+extern static int32_t bmp280_ut;
 
 static IO_t bmp280CsPin = IO_NONE;
 
