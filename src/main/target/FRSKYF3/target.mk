35a93303053b28cb1e145e848c6025524d364654
Paul Kurucz <paulkurucz@ge.com>
2017-09-02 19:19:21 -0400

    Build the ws2812 led and softserial on this platform

--- src/main/target/FRSKYF3/target.mk
+++ src/main/target/FRSKYF3/target.mk
@@ -9,4 +9,9 @@ TARGET_SRC = \
             drivers/compass/compass_ak8975.c \
             drivers/compass/compass_hmc5883l.c \
             drivers/accgyro/accgyro_mpu6050.c \
+            drivers/light_ws2811strip.c \
+            drivers/light_ws2811strip_stdperiph.c \
+            drivers/serial_softserial.c \
+            drivers/serial_usb_vcp.c \
+            drivers/rangefinder_hcsr04.c \
             drivers/max7456.c 
