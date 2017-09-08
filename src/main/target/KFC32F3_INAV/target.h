81a9d05f8667bd09ec0b3f59b6dba5b61240b2c9
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2017-09-08 21:10:08 +0200

    VTX disabled for KFC32 due to build problems

--- src/main/target/KFC32F3_INAV/target.h
+++ src/main/target/KFC32F3_INAV/target.h
@@ -149,3 +149,8 @@
 
 #define USABLE_TIMER_CHANNEL_COUNT 11
 #define USED_TIMERS             (TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4))
+
+#undef VTX_COMMON
+#undef VTX_CONTROL
+#undef VTX_SMARTAUDIO
+#undef VTX_TRAMP
