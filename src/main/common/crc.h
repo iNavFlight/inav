b6d085d120b3e3e01e4acf3f26a4c96d001b0112
Martin Budden <mjbudden@gmail.com>
2017-09-11 09:09:35 +0100

    Added XOR CRC functions

--- src/main/common/crc.h
+++ src/main/common/crc.h
@@ -28,3 +28,6 @@ void crc16_ccitt_sbuf_append(struct sbuf_s *dst, uint8_t *start);
 uint8_t crc8_dvb_s2(uint8_t crc, unsigned char a);
 uint8_t crc8_dvb_s2_update(uint8_t crc, const void *data, uint32_t length);
 void crc8_dvb_s2_sbuf_append(struct sbuf_s *dst, uint8_t *start);
+
+uint8_t crc8_xor_update(uint8_t crc, const void *data, uint32_t length);
+void crc8_xor_sbuf_append(struct sbuf_s *dst, uint8_t *start);
