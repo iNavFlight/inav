67689c930c67b15a3b3f4c7977646d3c81a7a05a
Alberto García Hierro <alberto@garciahierro.com>
2017-09-27 16:00:44 +0100

    Use OME_BoolFunc on cms_menu_ledstrip.c
    Allows removing a couple of static variables and replacing two
    functions with just one.

--- src/main/cms/cms_menu_ledstrip.c
+++ src/main/cms/cms_menu_ledstrip.c
@@ -44,35 +44,22 @@
 
 #ifdef LED_STRIP
 
-static bool featureRead = false;
-static uint8_t cmsx_FeatureLedstrip;
-
-static long cmsx_Ledstrip_FeatureRead(void)
+static bool cmsx_FeatureLedStrip_Enabled(bool *enabled)
 {
-    if (!featureRead) {
-        cmsx_FeatureLedstrip = feature(FEATURE_LED_STRIP) ? 1 : 0;
-        featureRead = true;
-    }
-
-    return 0;
-}
-
-static long cmsx_Ledstrip_FeatureWriteback(void)
-{
-    if (featureRead) {
-        if (cmsx_FeatureLedstrip)
+    if (enabled) {
+        if (*enabled) {
             featureSet(FEATURE_LED_STRIP);
-        else
+        } else {
             featureClear(FEATURE_LED_STRIP);
+        }
     }
-
-    return 0;
+    return featureConfigured(FEATURE_LED_STRIP);
 }
 
 static OSD_Entry cmsx_menuLedstripEntries[] =
 {
     { "-- LED STRIP --", OME_Label, NULL, NULL, 0 },
-    { "ENABLED",         OME_Bool,  NULL, &cmsx_FeatureLedstrip, 0 },
+    { "ENABLED",         OME_BoolFunc,  NULL, cmsx_FeatureLedStrip_Enabled, 0 },
 
     { "BACK", OME_Back, NULL, NULL, 0 },
     { NULL, OME_END, NULL, NULL, 0 }
@@ -81,9 +68,9 @@ static OSD_Entry cmsx_menuLedstripEntries[] =
 CMS_Menu cmsx_menuLedstrip = {
     .GUARD_text = "MENULED",
     .GUARD_type = OME_MENU,
-    .onEnter = cmsx_Ledstrip_FeatureRead,
+    .onEnter = NULL,
     .onExit = NULL,
-    .onGlobalExit = cmsx_Ledstrip_FeatureWriteback,
+    .onGlobalExit = NULL,
     .entries = cmsx_menuLedstripEntries
 };
 #endif // LED_STRIP
