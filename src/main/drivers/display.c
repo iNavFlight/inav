22a482787ac3290a02d08e306866820ec69d6c36
Alberto García Hierro <alberto@garciahierro.com>
2017-11-02 16:59:36 +0000

    Use a local buffer in displayWriteWithAttr() for attribute emulation
    This avoids calling into displayWriteCharWithAttr() for each
    character. Note that support for arbitrary length string is disabled
    behind a compile time option because it takes ~120 bytes of flash
    to support it and right now the biggest screen width we support is
    30 characters. Instead, we use a buffer which can hold up to 30
    characters to perform any required rewrites.

--- src/main/drivers/display.c
+++ src/main/drivers/display.c
@@ -30,12 +30,49 @@
 #include "display.h"
 
 #define SW_BLINK_CYCLE_MS 200 // 200ms on / 200ms off
+// XXX: This is the number of characters in a MAX7456 line.
+// Increment this number appropiately or enable support for
+// multiple iterations in displayWriteWithAttr() if bigger
+// displays are supported.
+#define DISPLAY_MAX_STRING_SIZE 30
 
 PG_REGISTER_WITH_RESET_TEMPLATE(displayConfig_t, displayConfig, PG_DISPLAY_CONFIG, 0);
 
 PG_RESET_TEMPLATE(displayConfig_t, displayConfig,
     .force_sw_blink = false,
 );
+
+static bool displayAttributesRequireEmulation(displayPort_t *instance, textAttributes_t attr)
+{
+    if (attr & ~instance->supportedTextAttributes) {
+        // We only emulate blink for now
+        return TEXT_ATTRIBUTES_HAVE_BLINK(attr);
+    }
+    return false;
+}
+
+static bool displayEmulateTextAttributes(displayPort_t *instance,
+                                        char *buf,
+                                        const char *s, size_t length,
+                                        textAttributes_t inAttr,
+                                        textAttributes_t *outAttr)
+{
+    UNUSED(instance);
+    UNUSED(s);
+
+    // We only emulate blink for now, so there's no need to test
+    // for it again.
+    TEXT_ATTRIBUTES_COPY(outAttr, &inAttr);
+    TEXT_ATTRIBUTES_REMOVE_BLINK(*outAttr);
+    if ((millis() / SW_BLINK_CYCLE_MS) % 2) {
+        memset(buf, ' ', length);
+        buf[length] = '\0';
+        // Tell the caller to use buf
+        return true;
+    }
+    // Tell the caller to use s but with outAttr rather than inAttr
+    return false;
+}
 
 void displayClearScreen(displayPort_t *instance)
 {
@@ -97,18 +134,43 @@ int displayWrite(displayPort_t *instance, uint8_t x, uint8_t y, const char *s)
 
 int displayWriteWithAttr(displayPort_t *instance, uint8_t x, uint8_t y, const char *s, textAttributes_t attr)
 {
-    if (attr & ~instance->supportedTextAttributes) {
-        // We can't overwrite s, so we write char by char
-        int ret;
-        for (unsigned ii = 0; ii < strlen(s); ii++) {
-            if ((ret = displayWriteCharWithAttr(instance, x + ii, y, s[ii], attr)) != 0) {
+    size_t length = strlen(s);
+
+    instance->posX = x + length;
+    instance->posY = y;
+
+    if (displayAttributesRequireEmulation(instance, attr)) {
+        // We can't overwrite s, so we use an intermediate buffer if we need
+        // text attribute emulation.
+        char buf[DISPLAY_MAX_STRING_SIZE + 1];
+        textAttributes_t blockAttr;
+        size_t blockSize = length > sizeof(buf) ? sizeof(buf) : length;
+        if (displayEmulateTextAttributes(instance, buf, s, blockSize, attr, &blockAttr)) {
+            // Emulation requires rewriting the string, which might be bigger than our buffer.
+            int ret = instance->vTable->writeString(instance, x, y, buf, blockAttr);
+#ifdef DISPLAY_ATTRIBUTE_EMULATION_MULTIPLE_WRITES
+            if (ret != 0) {
                 return ret;
             }
+            size_t written = blockSize;
+            while (written < length) {
+                size_t remaining = length - written;
+                blockSize = remaining > sizeof(buf) ? sizeof(buf) : remaining;
+                displayEmulateTextAttributes(instance, buf, s + written, blockSize, attr, &blockAttr);
+                ret = instance->vTable->writeString(instance, x + written, y, buf, blockAttr);
+                if (ret != 0) {
+                    return ret;
+                }
+                written += blockSize;
+            }
+#endif
+            return ret;
         }
-        return 0;
+
+        // Emulation at this time requires just changing the attributes, we can use the same string
+        TEXT_ATTRIBUTES_COPY(&attr, &blockAttr);
     }
-    instance->posX = x + strlen(s);
-    instance->posY = y;
+
     return instance->vTable->writeString(instance, x, y, s, attr);
 }
 
@@ -121,16 +183,11 @@ int displayWriteChar(displayPort_t *instance, uint8_t x, uint8_t y, uint8_t c)
 
 int displayWriteCharWithAttr(displayPort_t *instance, uint8_t x, uint8_t y, uint8_t c, textAttributes_t attr)
 {
+    if (displayAttributesRequireEmulation(instance, attr)) {
+        displayEmulateTextAttributes(instance, (char *)&c, (char *)&c, 1, attr, &attr);
+    }
     instance->posX = x + 1;
     instance->posY = y;
-    if (attr & ~instance->supportedTextAttributes) {
-        if (TEXT_ATTRIBUTES_HAVE_BLINK(attr)) {
-            if ((millis() / SW_BLINK_CYCLE_MS) % 2) {
-                c = ' ';
-            }
-            TEXT_ATTRIBUTES_REMOVE_BLINK(attr);
-        }
-    }
     return instance->vTable->writeChar(instance, x, y, c, attr);
 }
 
