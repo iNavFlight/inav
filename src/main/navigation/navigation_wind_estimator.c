6ff6c2212bffef29871b04591e0e1886ce333fbc
Alberto García Hierro <alberto@garciahierro.com>
2017-11-03 15:22:55 +0000

    Tweak the filtering for estimated wind speed a bit
    Remove conditional which allowed only negative or small positive
    changes to the estimated wind speed. Leave the current 0.95 and
    0.05 weights for past estimate/current estimate. Eventually we'll
    add a proper filter.

--- src/main/navigation/navigation_wind_estimator.c
+++ src/main/navigation/navigation_wind_estimator.c
@@ -153,16 +153,10 @@ void updateWindEstimator(timeUs_t currentTimeUs)
         wind[Y] = (groundVelocitySum[Y] - V * (sintheta * fuselageDirectionSum[X] + costheta * fuselageDirectionSum[Y])) * 0.5f;// equation 11
         wind[Z] = (groundVelocitySum[Z] - V * fuselageDirectionSum[Z]) * 0.5f;// equation 12
 
-        float wind_length = sqrtf(sq(wind[X]) + sq(wind[Y]) + sq(wind[Z]));
-        float _wind_length = sqrtf(sq(estimatedWind[X]) + sq(estimatedWind[Y]) + sq(estimatedWind[Z]));
-
-        // TODO: Why this conditional?
-        if (wind_length < _wind_length + 20) {
-            // TODO: Better filtering
-            estimatedWind[X] = estimatedWind[X] * 0.95f + wind[X] * 0.05f;
-            estimatedWind[Y] = estimatedWind[Y] * 0.95f + wind[Y] * 0.05f;
-            estimatedWind[Z] = estimatedWind[Z] * 0.95f + wind[Z] * 0.05f;
-        }
+        // TODO: Better filtering
+        estimatedWind[X] = estimatedWind[X] * 0.95f + wind[X] * 0.05f;
+        estimatedWind[Y] = estimatedWind[Y] * 0.95f + wind[Y] * 0.05f;
+        estimatedWind[Z] = estimatedWind[Z] * 0.95f + wind[Z] * 0.05f;
 
         lastUpdateUs = currentTimeUs;
         hasValidWindEstimate = true;
