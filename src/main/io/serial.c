990d48c2ca5846ad60bf113079b30a122ac18f42
Konstantin Sharlaimov (DigitalEntity) <konstantin.sharlaimov@gmail.com>
2017-11-17 21:49:23 +1000

    Initial support for FRSKYF4 target

--- src/main/io/serial.c
+++ src/main/io/serial.c
@@ -117,6 +117,15 @@ void pgResetFn_serialConfig(serialConfig_t *serialConfig)
 
     serialConfig->portConfigs[0].functionMask = FUNCTION_MSP;
 
+#ifdef USE_VCP
+    if (serialConfig->portConfigs[0].identifier == SERIAL_PORT_USB_VCP) {
+        serialPortConfig_t * uart1Config = serialFindPortConfiguration(SERIAL_PORT_USART1);
+        if (uart1Config) {
+            uart1Config->functionMask = FUNCTION_MSP;
+        }
+    }
+#endif
+
 #ifdef SERIALRX_UART
     serialPortConfig_t *serialRxUartConfig = serialFindPortConfiguration(SERIALRX_UART);
     if (serialRxUartConfig) {
@@ -128,15 +137,6 @@ void pgResetFn_serialConfig(serialConfig_t *serialConfig)
     serialPortConfig_t *gpsUartConfig = serialFindPortConfiguration(GPS_UART);
     if (gpsUartConfig) {
         gpsUartConfig->functionMask = FUNCTION_GPS;
-    }
-#endif
-
-#ifdef USE_VCP
-    if (serialConfig->portConfigs[0].identifier == SERIAL_PORT_USB_VCP) {
-        serialPortConfig_t * uart1Config = serialFindPortConfiguration(SERIAL_PORT_USART1);
-        if (uart1Config) {
-            uart1Config->functionMask = FUNCTION_MSP;
-        }
     }
 #endif
 
