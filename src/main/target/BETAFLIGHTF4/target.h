d20d0f91150fe681e68c92aadfc0ad464f5399d3
Michel Pastor <shellixyz@gmail.com>
2017-12-14 18:27:51 +0100

    BETAFLIGHTF4 draft r4: change PPM input timer

--- src/main/target/BETAFLIGHTF4/target.h
+++ src/main/target/BETAFLIGHTF4/target.h
@@ -158,4 +158,4 @@
 #define USABLE_TIMER_CHANNEL_COUNT 5
 
 //#define USED_TIMERS             ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(9) | TIM_N(10) )
-#define USED_TIMERS             ( TIM_N(3) | TIM_N(8) | TIM_N(10) )
+#define USED_TIMERS             ( TIM_N(3) | TIM_N(8) | TIM_N(4) )
