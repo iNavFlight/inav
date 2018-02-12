0fc7afe31d1c32c2049ba84f4ef423e7865ac3d6
giacomo892 <giacomo892@users.noreply.github.com>
2018-02-12 15:26:41 +0100

    Check if AUTOTRIM is enabled before arming and avoid arming.

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -541,6 +541,8 @@ static const char * osdArmingDisabledReasonMessage(void)
             return OSD_MESSAGE_STR("THROTTLE IS NOT LOW");
 	case ARMING_DISABLED_ROLLPITCH_NOT_CENTERED:
             return OSD_MESSAGE_STR("ROLLPITCH NOT CENTERED");
+        case ARMING_DISABLED_SERVO_AUTOTRIM:
+            return OSD_MESSAGE_STR("AUTOTRIM IS ACTIVE");
         case ARMING_DISABLED_CLI:
             return OSD_MESSAGE_STR("CLI IS ACTIVE");
             // Cases without message
