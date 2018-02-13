2f5575c351d0b35b8edcd2c0b3ebad137f3672d3
RcNet <ted@rcnet.com>
2018-02-13 13:08:03 +0100

    Fix YUPIF4 (#2461)
    * YUPIF4 fix led strip

--- src/main/target/YUPIF4/target.h
+++ src/main/target/YUPIF4/target.h
@@ -22,6 +22,7 @@
 
 #define LED0                    PB6
 #define LED1                    PB4
+#define LED2                    PB5
 
 #define BEEPER                  PC9
 #define BEEPER_PWM
@@ -113,7 +114,6 @@
 #define SDCARD_DETECT_PIN                   PD2
 #define SDCARD_SPI_INSTANCE                 SPI3
 #define SDCARD_SPI_CS_PIN                   PA15
-
 #define SDCARD_DMA_CHANNEL_TX               DMA1_Stream5
 #define SDCARD_DMA_CHANNEL_TX_COMPLETE_FLAG DMA_FLAG_TCIF5
 #define SDCARD_DMA_CLK                      RCC_AHB1Periph_DMA1
@@ -128,19 +128,19 @@
 #define VBAT_ADC_CHANNEL        ADC_CHN_1
 #define RSSI_ADC_CHANNEL        ADC_CHN_2
 
-// LED Strip can run off Pin 5 (PB1) of the motor outputs
-#define USE_LED_STRIP
-#define WS2811_PIN                      PB1
-#define WS2811_TIMER			TIM8
+// LED Strip can run off Pin 5 (PB0) of the motor outputs
+#define LED_STRIP
+#define WS2811_PIN                      PB0
+#define WS2811_TIMER			              TIM3
 #define WS2811_DMA_HANDLER_IDENTIFER    DMA1_ST2_HANDLER
 #define WS2811_DMA_STREAM               DMA1_Stream2
-#define WS2811_DMA_CHANNEL              DMA_Channel_5
+#define WS2811_DMA_CHANNEL              DMA_Channel_3
 
 // Features
 // #define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
-#define DEFAULT_FEATURES        FEATURE_BLACKBOX
+// #define DEFAULT_FEATURES        FEATURE_BLACKBOX
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
 
@@ -152,5 +152,5 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 8
-#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(8) | TIM_N(12))
+#define USABLE_TIMER_CHANNEL_COUNT 10
+#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) |TIM_N(8) | TIM_N(12))
