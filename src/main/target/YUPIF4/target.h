4eb0b3cfd358ab0ea96bf5a55d7d98c9e31acd32
root <ted@rcnet.com>
2018-02-24 03:43:00 +0100

    Fix Target YuPiF4

--- src/main/target/YUPIF4/target.h
+++ src/main/target/YUPIF4/target.h
@@ -22,13 +22,10 @@
 
 #define LED0                    PB6
 #define LED1                    PB4
-#define LED2                    PB5
 
 #define BEEPER                  PC9
 #define BEEPER_PWM
 #define BEEPER_INVERTED
-#define BEEPER_PWM_TIMER        TIM8
-#define BEEPER_PWM_TIMER_CH     TIM_Channel_4
 #define BEEPER_PWM_FREQUENCY    3150
 
 
@@ -41,56 +38,68 @@
 #define SPI1_MISO_PIN           PA6
 #define SPI1_MOSI_PIN           PA7
 
+#define USE_SPI
+#define USE_SPI_DEVICE_3 // SD Card
+#define SPI3_NSS_PIN            PA15
+#define SPI3_SCK_PIN            PC10
+#define SPI3_MISO_PIN           PC11
+#define SPI3_MOSI_PIN           PC12
+
 
 /////// ICM20689 ////////
 #define USE_EXTI
 #define MPU_INT_EXTI            PC4
+#define EXTI_CALLBACK_HANDLER_COUNT 1 // MPU data ready
 #define USE_MPU_DATA_READY_SIGNAL
 #define ENSURE_MPU_DATA_READY_IS_LOW
 
 // ICM20689
 #define MPU6500_CS_PIN          SPI1_NSS_PIN
-#define MPU6500_SPI_BUS         BUS_SPI1
+#define MPU6500_SPI_INSTANCE    SPI1
 
-#define USE_ACC
-#define USE_ACC_MPU6500
+#define ACC
+#define USE_ACC_SPI_MPU6500
 #define ACC_MPU6500_ALIGN       CW90_DEG
 
-#define USE_GYRO
-#define USE_GYRO_MPU6500
+#define GYRO
+#define USE_GYRO_SPI_MPU6500
 #define GYRO_MPU6500_ALIGN      CW90_DEG
 
 ////////////////////////
-
+/*
 #define USE_I2C
-#define USE_I2C_DEVICE_2
+#define I2C_DEVICE              (I2CDEV_2)
 #define I2C2_SCL                PB10
 #define I2C2_SDA                PB11
 
-#define USE_MAG
-#define MAG_I2C_BUS             BUS_I2C2
+#define MAG
 #define USE_MAG_HMC5883
 #define USE_MAG_QMC5883
-#define USE_MAG_IST8310
-#define USE_MAG_MAG3110
 
-#define USE_BARO
-#define BARO_I2C_BUS            BUS_I2C2
+#define BARO
 #define USE_BARO_BMP280
 #define USE_BARO_MS5611
+*/
 
 // OSD
-#define USE_OSD
+#define OSD
 #define USE_MAX7456
 #define MAX7456_SPI_BUS         BUS_SPI1
 #define MAX7456_CS_PIN          PA14
+#define MAX7456_SPI_CLK         (SPI_CLOCK_STANDARD) // 10MHz
+#define MAX7456_RESTORE_CLK     (SPI_CLOCK_FAST)
 
 // Serial Ports
+#define USB_IO
 #define USE_VCP
 
 #define USE_UART1
 #define UART1_RX_PIN            PA10
 #define UART1_TX_PIN            PA9
+
+#define USE_UART3
+#define UART3_RX_PIN            PB11
+#define UART3_TX_PIN            PB10
 
 #define USE_UART4
 #define UART4_RX_PIN            PC11
@@ -100,25 +109,25 @@
 #define UART6_RX_PIN            PC7
 #define UART6_TX_PIN            PC6
 
-#define SERIAL_PORT_COUNT       4 // VCP, UART1, UART3, UART6
+#define SERIAL_PORT_COUNT       5 // VCP, UART1, UART3, UART4, UART6
 
 #define USE_ESCSERIAL
 #define ESCSERIAL_TIMER_TX_HARDWARE 0
 
 
-// SD Card 
-/*
+// SD Card
 #define USE_SDCARD
 #define SDCARD_DETECT_INVERTED
 
 #define SDCARD_DETECT_PIN                   PD2
 #define SDCARD_SPI_INSTANCE                 SPI3
 #define SDCARD_SPI_CS_PIN                   PA15
+
 #define SDCARD_DMA_CHANNEL_TX               DMA1_Stream5
 #define SDCARD_DMA_CHANNEL_TX_COMPLETE_FLAG DMA_FLAG_TCIF5
 #define SDCARD_DMA_CLK                      RCC_AHB1Periph_DMA1
 #define SDCARD_DMA_CHANNEL                  DMA_Channel_0
-*/
+
 
 // ADC inputs
 #define BOARD_HAS_VOLTAGE_DIVIDER
@@ -128,19 +137,21 @@
 #define VBAT_ADC_CHANNEL        ADC_CHN_1
 #define RSSI_ADC_CHANNEL        ADC_CHN_2
 
-// LED Strip can run off Pin 5 (PB0) of the motor outputs
+// LED Strip can run off Pin 5 (PB1) of the motor outputs
 #define LED_STRIP
-#define WS2811_PIN                      PB0
-#define WS2811_TIMER			              TIM3
+#define WS2811_PIN                      PB1
+#define WS2811_TIMER                    TIM8
 #define WS2811_DMA_HANDLER_IDENTIFER    DMA1_ST2_HANDLER
 #define WS2811_DMA_STREAM               DMA1_Stream2
-#define WS2811_DMA_CHANNEL              DMA_Channel_3
+#define WS2811_DMA_CHANNEL              DMA_Channel_5
 
 // Features
 // #define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
-// #define DEFAULT_FEATURES        FEATURE_BLACKBOX
+#define SERIALRX_PROVIDER       SERIALRX_SBUS
+#define SERIALRX_UART           SERIAL_PORT_USART6
+#define DEFAULT_FEATURES        (FEATURE_BLACKBOX | FEATURE_VBAT | FEATURE_OSD)
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
 
@@ -152,5 +163,5 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 10
-#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) |TIM_N(8) | TIM_N(12))
+#define USABLE_TIMER_CHANNEL_COUNT 8
+#define USED_TIMERS             ( TIM_N(2) | TIM_N(3) | TIM_N(5) | TIM_N(8) | TIM_N(12))
