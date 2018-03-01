9edad3bc2a2bc65c75347dd593b311ac6b1c0160
Michel Pastor <shellixyz@gmail.com>
2018-03-01 01:07:32 +0100

    Improve stats: reformat + add total energy and average efficiency

--- src/main/fc/stats.h
+++ src/main/fc/stats.h
@@ -5,6 +5,9 @@
 typedef struct statsConfig_s {
     uint32_t stats_total_time; // [s]
     uint32_t stats_total_dist; // [m]
+#ifdef USE_ADC
+    uint32_t stats_total_energy; // deciWatt hour (x0.1Wh)
+#endif
     uint8_t  stats_enabled;
 } statsConfig_t;
 
