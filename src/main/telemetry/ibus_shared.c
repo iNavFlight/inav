399303f02a1a44beb3d10d2e9f6205d471df2274
hali9 <hali9@wp.pl>
2018-04-11 13:31:26 +0200

    ibus report cell voltage

--- src/main/telemetry/ibus_shared.c
+++ src/main/telemetry/ibus_shared.c
@@ -152,7 +152,11 @@ static uint8_t dispatchMeasurementRequest(ibusAddress_t address) {
     } else if (SENSOR_ADDRESS_TYPE_LOOKUP[address].value == IBUS_MEAS_VALUE_RPM) {
         return sendIbusMeasurement2(address, (uint16_t) (rcCommand[THROTTLE]));
     } else if (SENSOR_ADDRESS_TYPE_LOOKUP[address].value == IBUS_MEAS_VALUE_EXTERNAL_VOLTAGE) { //VBAT
-        return sendIbusMeasurement2(address, getBatteryVoltage());
+        if (telemetryConfig()->report_cell_voltage == 1) {
+            return sendIbusMeasurement2(address, getBatteryAverageCellVoltage());
+        } else {
+            return sendIbusMeasurement2(address, getBatteryVoltage());
+        }
     } else if (SENSOR_ADDRESS_TYPE_LOOKUP[address].value == IBUS_MEAS_VALUE_CURRENT) { //CURR in 10*mA, 1 = 10 mA
         if (feature(FEATURE_CURRENT_METER)) return sendIbusMeasurement2(address, (uint16_t) getAmperage()); //int32_t
         else return sendIbusMeasurement2(address, 0);
