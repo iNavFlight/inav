56a390fa45ccf0fdc203e5aded0db305fb5598e4
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2018-04-12 14:44:25 +0200

    Minor debug cleanup

--- src/main/navigation/navigation_multicopter.c
+++ src/main/navigation/navigation_multicopter.c
@@ -502,11 +502,9 @@ bool isMulticopterLandingDetected(void)
 
     bool possibleLandingDetected = isAtMinimalThrust && !verticalMovement && !horizontalMovement;
 
-    if (debugMode == DEBUG_NAV_LANDING_DETECTOR) {
-        debug[0] = isAtMinimalThrust * 100 + !verticalMovement * 10 + !horizontalMovement * 1;
-        debug[1] = (landingThrSamples == 0) ? 0 : (rcCommandAdjustedThrottle - (landingThrSum / landingThrSamples));
-        debug[2] = (currentTimeUs - landingTimer) / 1000;
-    }
+    DEBUG_SET(DEBUG_NAV_LANDING_DETECTOR, 0, isAtMinimalThrust * 100 + !verticalMovement * 10 + !horizontalMovement * 1);
+    DEBUG_SET(DEBUG_NAV_LANDING_DETECTOR, 1, (landingThrSamples == 0) ? 0 : (rcCommandAdjustedThrottle - (landingThrSum / landingThrSamples)));
+    DEBUG_SET(DEBUG_NAV_LANDING_DETECTOR, 2, (currentTimeUs - landingTimer) / 1000);
 
     // If we have surface sensor (for example sonar) - use it to detect touchdown
     if ((posControl.flags.estSurfaceStatus == EST_TRUSTED) && (posControl.actualState.surfaceMin >= 0)) {
