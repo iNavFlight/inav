9dba9440ba09ae89c329d8efaabd175f96a6d323
giacomo892 <giacomo892@users.noreply.github.com>
2018-04-16 15:20:03 +0200

    Fixes rc filtering logic

--- src/main/rx/rx.c
+++ src/main/rx/rx.c
@@ -523,9 +523,9 @@ void calculateRxChannelsAndUpdateFailsafe(timeUs_t currentTimeUs)
 
         // Update rcData channel value
         if (rxRuntimeConfig.requireFiltering) {
-            rcData[channel] = sample;
-        } else {
             rcData[channel] = applyChannelFiltering(channel, sample);
+        } else {
+            rcData[channel] = sample;
         }
     }
 
