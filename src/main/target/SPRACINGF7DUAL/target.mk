b222c0c430bec3daf9dadc428e37c64dc19a5604
Steffen Dirkwinkel <me@steffen.cc>
2018-10-02 12:42:53 +0200

    SPRACINGf7DUAL: port makefile and timers

--- src/main/target/SPRACINGF7DUAL/target.mk
+++ src/main/target/SPRACINGF7DUAL/target.mk
@@ -6,9 +6,8 @@ TARGET_SRC = \
             drivers/barometer/barometer_fake.c \
             drivers/compass/compass_fake.c \
             drivers/accgyro/accgyro_mpu6500.c \
-            drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/barometer/barometer_bmp280.c \
-            drivers/barometer/barometer_ms5611.c \
+            drivers/barometer/barometer_ms56xx.c \
             drivers/compass/compass_hmc5883l.c \
             drivers/max7456.c \
             drivers/vtx_rtc6705_soft_spi.c
