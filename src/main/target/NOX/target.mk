910a9a36abfb07752f052d23439bc687a244e058
jannes <jannes@mooiweertje.nl>
2019-01-16 21:08:49 +0100

    nox-board-support

--- src/main/target/NOX/target.mk
+++ src/main/target/NOX/target.mk
@@ -5,7 +5,6 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_mpu6000.c \
             drivers/barometer/barometer_bmp280.c \
             drivers/light_ws2811strip.c \
-            drivers/light_ws2811strip_stdperiph.c \
             drivers/flash_m25p16.c \
             drivers/max7456.c
 
