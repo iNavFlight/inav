3ceb2dccf7556ee3acb7793f0a80a4295ffc7503
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2019-02-03 14:23:09 +0100

    pwm outputs put in DTCM

--- src/main/drivers/pwm_output.c
+++ src/main/drivers/pwm_output.c
@@ -74,10 +74,10 @@ typedef struct {
 #endif
 } pwmOutputPort_t;
 
-static pwmOutputPort_t pwmOutputPorts[MAX_PWM_OUTPUT_PORTS];
+STATIC_FASTRAM pwmOutputPort_t pwmOutputPorts[MAX_PWM_OUTPUT_PORTS];
 
-static pwmOutputPort_t *motors[MAX_PWM_MOTORS];
-static pwmOutputPort_t *servos[MAX_PWM_SERVOS];
+STATIC_FASTRAM pwmOutputPort_t *motors[MAX_PWM_MOTORS];
+STATIC_FASTRAM pwmOutputPort_t *servos[MAX_PWM_SERVOS];
 
 #ifdef USE_DSHOT
 
