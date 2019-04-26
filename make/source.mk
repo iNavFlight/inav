bc3d857693568321771c12d6eaa4f1402531298b
Alberto García Hierro <alberto@garciahierro.com>
2019-04-26 15:32:38 +0100

    Remove duplicate entries from source.mk
    Introduced accidentaly while rebasing

--- make/source.mk
+++ make/source.mk
@@ -150,8 +150,6 @@ COMMON_SRC = \
             cms/cms_menu_vtx_smartaudio.c \
             cms/cms_menu_vtx_tramp.c \
             cms/cms_menu_vtx_ffpv.c \
-            common/colorconversion.c \
-            common/gps_conversion.c \
             drivers/display_ug2864hsweg01.c \
             drivers/rangefinder/rangefinder_hcsr04.c \
             drivers/rangefinder/rangefinder_hcsr04_i2c.c \
