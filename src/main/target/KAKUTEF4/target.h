84e0f7c94173a769012f9438b52ba55e4c7b9764
jihlein <jihlein@hotmail.com>
2019-06-21 07:21:59 -0600

    Add KakuteF4V2 tricopter build configuration/target

--- src/main/target/KAKUTEF4/target.h
+++ src/main/target/KAKUTEF4/target.h
@@ -23,13 +23,15 @@
  */
 
 #pragma once
-#if defined(KAKUTEF4V2)
+#if defined(KAKUTEF4V2) || defined(KAKUTEF4V2TRI)
 #   define TARGET_BOARD_IDENTIFIER "KTV2"
 #   define USBD_PRODUCT_STRING "KakuteF4-V2"
 #else
 #   define TARGET_BOARD_IDENTIFIER "KTV1"
 #   define USBD_PRODUCT_STRING "KakuteF4-V1"
 #endif
+
+#define USE_TARGET_CONFIG
 
 #define LED0                    PB5
 #define LED1                    PB4
@@ -53,7 +55,7 @@
 #define USE_ACC_MPU6500
 #define ACC_MPU6500_ALIGN       CW270_DEG
 
-#ifdef KAKUTEF4V2
+#if defined(KAKUTEF4V2) || defined(KAKUTEF4V2TRI)
 #   define USE_I2C
 #   define USE_I2C_DEVICE_1
 #   define I2C1_SCL                PB8        // SCL pad
@@ -68,8 +70,6 @@
 #   define USE_MAG_IST8310
 #   define USE_MAG_IST8308
 #   define USE_MAG_LIS3MDL
-
-#   define TEMPERATURE_I2C_BUS     BUS_I2C1
 
 #   define USE_BARO
 #   define BARO_I2C_BUS            BUS_I2C1
@@ -108,7 +108,7 @@
 #define UART6_RX_PIN            PC7
 #define UART6_TX_PIN            PC6
 
-#ifdef KAKUTEF4V2
+#if defined(KAKUTEF4V2) || defined(KAKUTEF4V2TRI)
 #   define USE_UART4
 #   define UART4_RX_PIN            PA1
 #   define UART4_TX_PIN            PA0
@@ -147,10 +147,17 @@
 #define ADC_CHANNEL_3_PIN           PC1
 
 #define USE_LED_STRIP
+#if !defined(KAKUTEF4V2TRI)
 #define WS2811_PIN                      PC8
 #define WS2811_DMA_HANDLER_IDENTIFER    DMA2_ST4_HANDLER
 #define WS2811_DMA_STREAM               DMA2_Stream4
 #define WS2811_DMA_CHANNEL              DMA_Channel_7
+#else
+#define WS2811_PIN                      PA2
+#define WS2811_DMA_HANDLER_IDENTIFER    DMA1_ST0_HANDLER
+#define WS2811_DMA_STREAM               DMA1_Stream0
+#define WS2811_DMA_CHANNEL              DMA_Channel_6
+#endif
 
 #define VBAT_ADC_CHANNEL            ADC_CHN_1
 #define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
@@ -170,10 +177,10 @@
 #define TARGET_IO_PORTD        (BIT(2))
 
 #define USE_DSHOT
-#define USE_SERIALSHOT
+#define USE_SERIAL_SHOT
 #define USE_ESC_SENSOR
 
-#ifdef KAKUTEF4V2
+#if defined(KAKUTEF4V2) || defined(KAKUTEF4V2TRI)
 #   define MAX_PWM_OUTPUT_PORTS       4
 #else
 #   define MAX_PWM_OUTPUT_PORTS       6
