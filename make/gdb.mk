48b921be0ecd555d9b70c22136a456a0b6022d1c
Alberto García Hierro <alberto@garciahierro.com>
2019-11-04 16:19:09 +0000

    [DOC] Document hardware debugging procedure with stlink, openocd and gdb

--- make/gdb.mk
+++ make/gdb.mk
@@ -1,7 +1,8 @@
 GDB ?= $(ARM_SDK_PREFIX)gdb
-GDB_OPENOCD_REMOTE ?= localhost:3333
+GDB_REMOTE ?= localhost:3333
 
-GDB_OPENOCD_INIT_CMDS ?= -ex "monitor reset halt"
+GDB_OPENOCD_INIT_CMDS ?=
+GDB_OPENOCD_INIT_CMDS += -ex "monitor reset halt"
 ifneq ($(LOAD),)
 GDB_OPENOCD_INIT_CMDS += -ex load
 endif
@@ -10,6 +11,4 @@ GDB_OPENOCD_INIT_CMDS += -ex "monitor arm semihosting enable"
 endif
 
 gdb-openocd: $(TARGET_ELF)
-	(nc -z $(subst :, ,$(GDB_OPENOCD_REMOTE)) 2> /dev/null && \
-	$(GDB) $< -ex "target remote $(GDB_OPENOCD_REMOTE)" $(GDB_OPENOCD_INIT_CMDS)) || \
-	$(GDB) $< -ex "target remote | $(OPENOCD_CMDLINE) -c \"gdb_port pipe;\"" $(GDB_OPENOCD_INIT_CMDS)
+	$(GDB) $< -ex "target remote $(GDB_REMOTE)" $(GDB_OPENOCD_INIT_CMDS)
