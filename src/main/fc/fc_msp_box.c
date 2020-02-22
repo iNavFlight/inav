14f42f15d728e4ba9b0d41b02919ecc2547b0574
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2020-02-22 14:59:48 +0100

    Disable POSHOLD for Rovers and Boats

--- src/main/fc/fc_msp_box.c
+++ src/main/fc/fc_msp_box.c
@@ -198,7 +198,9 @@ void initActiveBoxIds(void)
     const bool navReadyOther = !STATE(MULTIROTOR) && sensors(SENSOR_ACC) && feature(FEATURE_GPS);
     const bool navFlowDeadReckoning = sensors(SENSOR_OPFLOW) && sensors(SENSOR_ACC) && positionEstimationConfig()->allow_dead_reckoning;
     if (navFlowDeadReckoning || navReadyMultirotor || navReadyOther) {
-        activeBoxIds[activeBoxIdCount++] = BOXNAVPOSHOLD;
+        if (!STATE(ROVER) && !STATE(BOAT)) {
+            activeBoxIds[activeBoxIdCount++] = BOXNAVPOSHOLD;
+        }
         if (STATE(AIRPLANE)) {
             activeBoxIds[activeBoxIdCount++] = BOXLOITERDIRCHN;
         }
