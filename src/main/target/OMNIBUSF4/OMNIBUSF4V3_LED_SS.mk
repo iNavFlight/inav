a4fa8816552731a98a29f75f06a87d760cfbc49a
Esteban16899 <40577855+Esteban16899@users.noreply.github.com>
2020-03-29 22:14:18 -0300

    Added OMNIBUSF4V3_LED_SS target file

---
+++ src/main/target/OMNIBUSF4/OMNIBUSF4V3_LED_SS.mk
@@ -0,0 +1,3 @@
+# OMNIBUSF4V3_LED_SS is a (almost identical) variant of OMNIBUSF4PRO target,
+# except for a SOFTSERIAL1 UART6 and SOFTSERIAL2 (RX&TX) on LEDSTIRP PIN (PB6)
+FEATURES       = VCP SDCARD MSC
