iNavFlight/inav

commit fce005f7f988b43ff8c74908a40483607130bbda

Switch compiler to _Ofast for critical files

src/main/drivers/max7456.c
@@ -22,6 +22,8 @@
 
 #include "platform.h"
 
+FILE_COMPILE_FOR_SPEED
+
 #ifdef USE_MAX7456
 
 #include "common/bitarray.h"

src/main/common/encoding.c
@@ -15,6 +15,10 @@
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "platform.h"
+
+FILE_COMPILE_FOR_SPEED
+
 #include "encoding.h"
 
 /**

src/main/common/filter.c
@@ -22,6 +22,8 @@
 
 #include "platform.h"
 
+FILE_COMPILE_FOR_SPEED
+
 #include "common/filter.h"
 #include "common/maths.h"
 #include "common/utils.h"

src/main/common/maths.c
@@ -25,6 +25,8 @@
 #include "quaternion.h"
 #include "platform.h"
 
+FILE_COMPILE_FOR_SPEED
+
 // http://lolengine.net/blog/2011/12/21/better-function-approximations
 // Chebyshev http://stackoverflow.com/questions/345085/how-do-trigonometric-functions-work/345117#345117
 // Thanks for ledvinap for making such accuracy possible! See: https://github.com/cleanflight/cleanflight/issues/940#issuecomment-110323384

src/main/common/typeconversion.c
@@ -20,6 +20,8 @@
 #include "build/build_config.h"
 
 #include "maths.h"
+#include "platform.h"
+FILE_COMPILE_FOR_SPEED
 
 #ifdef REQUIRE_PRINTF_LONG_SUPPORT
 

src/main/drivers/pwm_output.c
@@ -22,6 +22,8 @@
 
 #include "platform.h"
 
+FILE_COMPILE_FOR_SPEED
+
 #include "build/debug.h"
 
 #include "common/log.h"

src/main/fc/fc_core.c
@@ -21,6 +21,8 @@
 
 #include "platform.h"
 
+FILE_COMPILE_FOR_SPEED
+
 #include "blackbox/blackbox.h"
 
 #include "build/debug.h"

src/main/flight/gyroanalyse.c
@@ -26,6 +26,7 @@
 #include <stdint.h>
 
 #include "platform.h"
+FILE_COMPILE_FOR_SPEED
 
 #ifdef USE_DYNAMIC_FILTERS
 

commit a6985fd393528bed860a1634efac75639fe43011

Fix OSD hang due to compiler optimization bug
Introduced by fce005f

Fixes #5690

src/main/drivers/max7456.c
@@ -22,8 +22,6 @@
 
 #include "platform.h"
 
-FILE_COMPILE_FOR_SPEED
-
 #ifdef USE_MAX7456
 
 #include "common/bitarray.h"