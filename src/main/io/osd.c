6976d464c8e866f316fb752ac1697eb6e85379a5
Darren Lines <darren@darrenlines.uk>
2020-05-10 16:21:07 +0100

    Removed Air from OSD mode display

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -1655,8 +1655,6 @@ static bool osdDrawSingleElement(uint8_t item)
                 p = "ANGL";
             else if (FLIGHT_MODE(HORIZON_MODE))
                 p = "HOR ";
-            else if (STATE(AIRMODE_ACTIVE))
-                p = "AIR ";
 
             displayWrite(osdDisplayPort, elemPosX, elemPosY, p);
             return true;
