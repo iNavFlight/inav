cf7b3be6eefc2415c00f293c1518416020da97aa
Hydra <me@dominicclifton.name>
2020-06-10 17:24:24 +0200

    Add stub drivers/system32h7xx.c

---
+++ src/main/drivers/system_stm32h7xx.c
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Cleanflight.
+ *
+ * Cleanflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cleanflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "platform.h"
+
+#include "drivers/accgyro/accgyro_mpu.h"
+#include "drivers/exti.h"
+#include "drivers/nvic.h"
+#include "drivers/system.h"
+
+void SystemClock_Config(void);
+
+void enableGPIOPowerUsageAndNoiseReductions(void)
+{
+    // TODO
+}
+
+bool isMPUSoftReset(void)
+{
+    // TODO
+}
+
+uint32_t systemBootloaderAddress(void)
+{
+    // TODO
+}
+
+void systemClockSetup(uint8_t cpuUnderclock)
+{
+    // TODO
+}
+
+void systemInit(void)
+{
+    // TODO
+}
