3a6cdff181bff0292ddc3c378ce2c78f95418070
Konstantin (DigitalEntity) Sharlaimov <konstantin.sharlaimov@gmail.com>
2020-07-31 19:35:56 +0200

    [BMP280] Allow target-specific I2C address definition

--- src/main/target/GRAVITYF7/target.c
+++ src/main/target/GRAVITYF7/target.c
@@ -27,8 +27,6 @@
 #include "drivers/bus.h"
 #include "drivers/sensor.h"
 
-BUSDEV_REGISTER_I2C( busdev_bmp280_2, DEVHW_BMP280, BARO_I2C_BUS, 0x77, NONE, DEVFLAGS_NONE, 0 );
-
 const timerHardware_t timerHardware[] = {
     DEF_TIM(TIM4, CH2, PB7,  TIM_USE_PPM,   0, 0),       // PPM&SBUS
 
