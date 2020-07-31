03a014d0e1684d6ae44ca8f06aca5081ed78ffed
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2020-07-31 10:30:16 +0200

    Bump the number of logic conditions to 32

--- src/main/programming/logic_condition.c
+++ src/main/programming/logic_condition.c
@@ -49,7 +49,7 @@
 #include "io/vtx.h"
 #include "drivers/vtx_common.h"
 
-PG_REGISTER_ARRAY_WITH_RESET_FN(logicCondition_t, MAX_LOGIC_CONDITIONS, logicConditions, PG_LOGIC_CONDITIONS, 1);
+PG_REGISTER_ARRAY_WITH_RESET_FN(logicCondition_t, MAX_LOGIC_CONDITIONS, logicConditions, PG_LOGIC_CONDITIONS, 2);
 
 EXTENDED_FASTRAM uint64_t logicConditionsGlobalFlags;
 EXTENDED_FASTRAM int logicConditionValuesByType[LOGIC_CONDITION_LAST];
