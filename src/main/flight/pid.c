a53b1c061947b2624e58df5e29847163b252f89c
Andi Kanzler <andi_kanzler@gmx.de>
2020-08-08 22:42:03 +0200

    Add FF to inflight adjustments

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -1114,3 +1114,12 @@ const pidBank_t * pidBank(void) {
 pidBank_t * pidBankMutable(void) { 
     return usedPidControllerType == PID_TYPE_PIFF ? &pidProfileMutable()->bank_fw : &pidProfileMutable()->bank_mc;
 }
+
+uint8_t * getD_FFRefByBank(pidBank_t *pidBank, pidIndex_e pidIndex)
+{
+    if (pidIndexGetType(pidIndex) == PID_TYPE_PIFF) {
+       return &pidBank->pid[pidIndex].FF;
+    } else {
+        return &pidBank->pid[pidIndex].D;
+    }
+}
