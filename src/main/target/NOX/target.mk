93f637313ad635ae97f5f5e78402c800de8d3393
Alberto García Hierro <alberto@garciahierro.com>
2020-09-18 19:07:14 +0100

    [CMAKE] Remove old Makefile based build system

--- src/main/target/NOX/target.mk
+++
@@ -1,9 +0,0 @@
-F411_TARGETS    += $(TARGET)
-FEATURES        += VCP ONBOARDFLASH MSC
-
-TARGET_SRC = \
-            drivers/accgyro/accgyro_mpu6000.c \
-            drivers/barometer/barometer_bmp280.c \
-            drivers/light_ws2811strip.c \
-            drivers/flash_m25p16.c \
-            drivers/max7456.c
