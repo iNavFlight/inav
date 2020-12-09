2a01ee0515cb23eaa4e818e628836c0b55cb892e
FastJack2 <fj2@fastjack2.net>
2020-12-09 10:12:11 +0100

    Added Support for Lumenier Lux Mini F7

---
+++ src/main/target/LUXMINIF7/target.c
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Cleanflight and Betaflight.
+ *
+ * Cleanflight and Betaflight are free software. You can redistribute
+ * this software and/or modify this software under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Cleanflight and Betaflight are distributed in the hope that they
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+
+#include "platform.h"
+
+#include "fc/fc_msp.h"
+
+#include "drivers/dma.h"
+#include "drivers/barometer/barometer.h"
+
+#include "drivers/bus.h"
+#include "drivers/io.h"
+#include "drivers/pwm_mapping.h"
+#include "drivers/timer.h"
+#include "drivers/pinio.h"
+#include "drivers/sensor.h"
+
+#include "drivers/dma.h"
+#include "drivers/barometer/barometer.h"
+
+
+BUSDEV_REGISTER_SPI_TAG(busdev_mpu6000,     DEVHW_MPU6000,      MPU6000_SPI_BUS,    MPU6000_CS_PIN,     MPU6000_EXTI_PIN,       0,  DEVFLAGS_NONE,  IMU_MPU6000_ALIGN);
+
+const timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM2, CH1, PA15, TIM_USE_LED,           0, 0), // ONBOARD LED
+
+    DEF_TIM(TIM1, CH1, PA8, TIM_USE_ANY,            0, 0), // CAM CONTROL
+
+    // MOTORS
+    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_MC_MOTOR,         0, 0), //S1
+    DEF_TIM(TIM8, CH3, PC8,  TIM_USE_MC_MOTOR,         0, 0), //S2
+    DEF_TIM(TIM4, CH2, PB7,  TIM_USE_MC_MOTOR,         0, 0), //S3
+    DEF_TIM(TIM8, CH4, PC9,  TIM_USE_MC_MOTOR,         0, 0), //S4
+};
+
+const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
