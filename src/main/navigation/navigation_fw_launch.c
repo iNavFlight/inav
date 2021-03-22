3a82e26b8bdefa00df6b30aaee78ab703778508b
Alexander van Saase <avsaase@gmail.com>
2021-03-22 21:58:23 +0100

    add groundspeed check

--- src/main/navigation/navigation_fw_launch.c
+++ src/main/navigation/navigation_fw_launch.c
@@ -318,7 +318,7 @@ static fixedWingLaunchEvent_t fwLaunchState_FW_LAUNCH_STATE_WAIT_DETECTION(timeU
 
     const bool isBungeeLaunched = isForwardAccelerationHigh && isAircraftAlmostLevel;
     const bool isSwingLaunched = (swingVelocity > navConfig()->fw.launch_velocity_thresh) && (imuMeasuredAccelBF.x > 0);
-    const bool isForwardLaunched = isGPSHeadingValid() && (imuMeasuredAccelBF.x > 0);
+    const bool isForwardLaunched = isGPSHeadingValid() && (gpsSol.groundSpeed > navConfig()->fw.launch_velocity_thresh) && (imuMeasuredAccelBF.x > 0);
 
     applyThrottleIdleLogic(false);
 
