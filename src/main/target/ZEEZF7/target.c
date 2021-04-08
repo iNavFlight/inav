c7b9637a0b8909799bf512c4f0056c100bc32fb1
Niccolò Maggioni <nicco.maggioni@gmail.com>
2021-04-08 17:23:01 +0200

    Merge ZEEZF7 and ZEEZF7V2 targets

--- src/main/target/ZEEZF7/target.c
+++ src/main/target/ZEEZF7/target.c
@@ -25,10 +25,21 @@
 #include "drivers/timer.h"
 
 const timerHardware_t timerHardware[] = {
+#ifdef ZEEZF7V2
+    DEF_TIM(TIM4, CH2,  PB7, TIM_USE_MC_MOTOR, 0, 0), // S1
+    DEF_TIM(TIM4, CH1,  PB6, TIM_USE_MC_MOTOR, 0, 0), // S2
+    DEF_TIM(TIM3, CH1,  PB4, TIM_USE_MC_MOTOR, 0, 0), // S3
+    DEF_TIM(TIM2, CH2,  PB3, TIM_USE_MC_MOTOR, 0, 0), // S4
+    DEF_TIM(TIM3, CH3,  PC8, TIM_USE_MC_MOTOR, 0, 0), // S5
+    DEF_TIM(TIM3, CH2,  PC7, TIM_USE_MC_MOTOR, 0, 0), // S6
+    DEF_TIM(TIM8, CH1,  PC6, TIM_USE_MC_MOTOR, 0, 0), // S7
+    DEF_TIM(TIM3, CH4,  PB1, TIM_USE_MC_MOTOR, 0, 0), // S8
+#else
     DEF_TIM(TIM4, CH3,  PB8, TIM_USE_MC_MOTOR, 0, 0), // S1
     DEF_TIM(TIM4, CH2,  PB7, TIM_USE_MC_MOTOR, 0, 0), // S2
     DEF_TIM(TIM4, CH1,  PB6, TIM_USE_MC_MOTOR, 0, 0), // S3
     DEF_TIM(TIM3, CH1,  PB4, TIM_USE_MC_MOTOR, 0, 0), // S4
+#endif
 
     DEF_TIM(TIM1, CH2N, PB0, TIM_USE_LED     , 0, 0), // LED STRIP
 };
