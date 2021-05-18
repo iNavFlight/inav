26d0f3f67efe37c1f4ca2b1ec795ade3f718beb0
breadoven <56191411+breadoven@users.noreply.github.com>
2021-05-18 11:08:57 +0100

    Revert "Fix OSD WP number bug at mission end"
    This reverts commit 785ce75225ce66e92a4a5ca979d1567adcb186da.

--- src/main/navigation/navigation.c
+++ src/main/navigation/navigation.c
@@ -1451,9 +1451,8 @@ static navigationFSMEvent_t navOnEnteringState_NAV_STATE_WAYPOINT_INITIALIZE(nav
   Using p3 minimises the risk of saving an invalid counter if a mission is aborted.
 */
         static bool missionRestart;
-        if (posControl.activeWaypointIndex == 0 || posControl.flags.wpMissionFinished) {
+        if (posControl.activeWaypointIndex == 0) {
             missionRestart = true;
-            posControl.flags.wpMissionFinished = false;
         } else if (navConfig()->general.flags.waypoint_mission_restart == WP_MISSION_SWITCH) {
             missionRestart = !missionRestart;
         } else {
@@ -1534,7 +1533,7 @@ static navigationFSMEvent_t navOnEnteringState_NAV_STATE_WAYPOINT_PRE_ACTION(nav
             return nextForNonGeoStates();
 
         case NAV_WP_ACTION_RTH:
-            posControl.flags.wpMissionFinished = true;
+            posControl.activeWaypointIndex = 0;
             return NAV_FSM_EVENT_SWITCH_TO_RTH;
     };
 
@@ -1675,7 +1674,7 @@ static navigationFSMEvent_t navOnEnteringState_NAV_STATE_WAYPOINT_FINISHED(navig
     UNUSED(previousState);
 
     clearJumpCounters();
-    posControl.flags.wpMissionFinished = true;
+    posControl.activeWaypointIndex = 0;
 
     // If no position sensor available - land immediately
     if ((posControl.flags.estPosStatus >= EST_USABLE) && (posControl.flags.estHeadingStatus >= EST_USABLE)) {
