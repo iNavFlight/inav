76513df04154df1f173e719659facf84e5bc8f7d
ligenxxxx <59721724+ligenxxxx@users.noreply.github.com>
2021-05-26 13:45:49 +0800

    Update serial.h

--- src/main/io/serial.h
+++ src/main/io/serial.h
@@ -55,7 +55,7 @@ typedef enum {
     FUNCTION_DJI_HD_OSD                 = (1 << 21), // 2097152
     FUNCTION_SERVO_SERIAL               = (1 << 22), // 4194304
     FUNCTION_TELEMETRY_SMARTPORT_MASTER = (1 << 23), // 8388608
-    FUNCTION_IMU2                       = (1 << 24), // 16777216
+    FUNCTION_IMU2                      = (1 << 24), // 16777216
     FUNCTION_MSP_DISPLAYPORT            = (1 << 25), // 33554432
 } serialPortFunction_e;
 
