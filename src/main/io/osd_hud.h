ccd9aa51e54f34b3cb5516f91f7cd9109e36f893
Olivier C <olivierc600@gmail.com>
2021-06-02 19:51:10 +0200

    OSD radar, add relative altitude and remove nearest POI extras
    - Every 7 seconds, the relative altitude of the other POIs is displayed instead of the distance. Works for radar and waypoints.
    - Removal of the extra display for the nearest POI (was mostly used for debug)

--- src/main/io/osd_hud.h
+++ src/main/io/osd_hud.h
@@ -16,8 +16,8 @@
  */
 
 #pragma once
-
 #include <stdint.h>
+#include "drivers/osd_symbols.h"
 
 typedef struct displayCanvas_s displayCanvas_t;
 
@@ -26,6 +26,5 @@ void osdHudClear(void);
 void osdHudDrawCrosshair(displayCanvas_t *canvas, uint8_t px, uint8_t py);
 void osdHudDrawHoming(uint8_t px, uint8_t py);
 void osdHudDrawPoi(uint32_t poiDistance, int16_t poiDirection, int32_t poiAltitude, uint8_t poiType, uint16_t poiSymbol, int16_t poiP1, int16_t poiP2);
-void osdHudDrawExtras(uint8_t poi_id);
 int8_t radarGetNearestPOI(void);
 
