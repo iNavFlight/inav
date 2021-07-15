2e1fd89f0b8b35197c5587d2c228b8698919a044
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2021-07-15 15:38:28 +0200

    SpeedyBeeF7V2 target

---
+++ src/main/target/SPEEDYBEEF7V2/target.c
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Cleanflight and Betaflight.
+ *
+ * Cleanflight and Betaflight are free software. You can redistribute
+ * this software and/or modify this software under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Cleanflight and Betaflight are distributed in the hope that they
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+
+#include <platform.h>
+#include "drivers/io.h"
+#include "drivers/pwm_mapping.h"
+#include "drivers/timer.h"
+#include "drivers/bus.h"
+
+const timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM4, CH2, PB7,  TIM_USE_PPM,   0, 0),  // PPM
+
+    DEF_TIM(TIM1, CH1, PA8,     TIM_USE_MC_MOTOR | TIM_USE_FW_MOTOR, 0, 1),  // S1
+    DEF_TIM(TIM1, CH2, PA9,     TIM_USE_MC_MOTOR | TIM_USE_FW_MOTOR, 0, 1),  // S2
+    DEF_TIM(TIM1, CH3, PA10,    TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO, 0, 1),  // S3
+    DEF_TIM(TIM3, CH4, PC9,     TIM_USE_MC_MOTOR | TIM_USE_FW_SERVO, 0, 0),  // S4
+
+    DEF_TIM(TIM8, CH2N, PB0,  TIM_USE_LED, 0, 0),    // LED
+
+    DEF_TIM(TIM5, CH1, PA0,  TIM_USE_ANY, 0, 0), // Camera Control
+};
+
+const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
