cdf318176a05bc5619a934bd8206d8e159f9429d
breadoven <56191411+breadoven@users.noreply.github.com>
2021-09-20 08:57:51 +0100

    Update navigation.c
    Make failsafe applicable to WP mission continuation as well as RTH

--- src/main/navigation/navigation.c
+++ src/main/navigation/navigation.c
@@ -3189,14 +3189,14 @@ static navigationFSMEvent_t selectNavEventFromBoxModeInput(void)
         }
 
         /* Keep Emergency landing mode active once triggered. Is cancelled when landing in progress if position sensors working again.
-         * If failsafe RTH not active landing also cancelled if WP or RTH deselected or if Manual or Althold modes selected.
+         * If failsafe not active landing also cancelled if WP or RTH deselected or if Manual or Althold modes selected.
          * Remains active if landing finished regardless of sensor status or flight mode selection */
         bool autonomousNavNotPossible = !(canActivateNavigation && canActivateAltHold && STATE(GPS_FIX_HOME));
         bool emergLandingCancel = IS_RC_MODE_ACTIVE(BOXMANUAL) || (IS_RC_MODE_ACTIVE(BOXNAVALTHOLD) && canActivateAltHold) ||
                                   !(IS_RC_MODE_ACTIVE(BOXNAVWP) || IS_RC_MODE_ACTIVE(BOXNAVRTH));
 
         if (navigationIsExecutingAnEmergencyLanding()) {
-            if (autonomousNavNotPossible && (!emergLandingCancel || posControl.flags.forcedRTHActivated)) {
+            if (autonomousNavNotPossible && (!emergLandingCancel || FLIGHT_MODE(FAILSAFE_MODE))) {
                 return NAV_FSM_EVENT_SWITCH_TO_EMERGENCY_LANDING;
             }
         }
