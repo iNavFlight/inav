0804a0446ba9e9d4c96165837e53e534e02a9a5b
Jonathan Hudson <jh+github@daria.co.uk>
2021-10-17 14:25:59 +0100

    make max WP 120 (#7510)

--- src/main/target/common.h
+++ src/main/target/common.h
@@ -79,7 +79,7 @@
 
 #if (MCU_FLASH_SIZE > 256)
 
-#if defined(MAG_I2C_BUS) || defined(VCM5883_I2C_BUS)    
+#if defined(MAG_I2C_BUS) || defined(VCM5883_I2C_BUS)
 #define USE_MAG_VCM5883
 #endif
 
@@ -190,7 +190,7 @@
 //#define USE_MSP_RC_OVERRIDE
 #define USE_SERIALRX_CRSF
 #define USE_SERIAL_PASSTHROUGH
-#define NAV_MAX_WAYPOINTS       60
+#define NAV_MAX_WAYPOINTS       120
 #define USE_RCDEVICE
 
 //Enable VTX control
