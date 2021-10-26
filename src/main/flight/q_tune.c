a786f8713e2784509362cabeea779232a46b107b
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2021-10-26 09:56:33 +0200

    Basic osicllation detector

--- src/main/flight/q_tune.c
+++ src/main/flight/q_tune.c
@@ -30,6 +30,7 @@ FILE_COMPILE_FOR_SPEED
 #include "common/filter.h"
 #include "build/debug.h"
 #include "fc/controlrate_profile.h"
+#include "fc/rc_controls.h"
 
 #define Q_TUNE_SHORT_BUFFER_PERIOD_MS 350
 #define Q_TUNE_LONG_BUFFER_PERIOD_MS  800
@@ -37,13 +38,26 @@ FILE_COMPILE_FOR_SPEED
 #define Q_TUNE_SHORT_BUFFER_LENGTH 64 // Q_TUNE_UPDATE_RATE_HZ * Q_TUNE_SHORT_BUFFER_PERIOD_MS / 1000
 #define Q_TUNE_LONG_BUFFER_LENGTH 128  // Q_TUNE_UPDATE_RATE_HZ * Q_TUNE_LONG_BUFFER_PERIOD_MS / 1000
 
+#define Q_TUNE_HI_FREQ_THRESHOLD 12 // Osciallation above this frequency means we do vibrate and PID is not stable
+#define Q_TUNE_HI_FREQ_EVENT_THRESHOLD_US 150000 // If we have a high frequency event for longer than this, we do vibrate 
+#define Q_TUNE_HI_FREQ_EVENT_PERIOD_US 1000000 // Hi Freq event can happen only from time to time
+
+
 typedef struct currentSample_s {
     float setpoint;
     float measurement;
     float iTerm;
 } currentSample_t;
 
+typedef enum {
+    Q_TUNE_STATE_HI_FREQ_OSCILLATION        = (1 << 0),
+    Q_TUNE_STATE_LOW_FREQ_OSCILLATION       = (1 << 1),
+    Q_TUNE_STATE_HI_FREQ_START              = (1 << 2),   // start of a high frequency oscillation    
+} qTuneState_e;
+
 typedef struct samples_s {
+    uint8_t state;
+
     timeUs_t lastExecution;
     uint16_t indexShort;
     uint16_t indexLong;
@@ -75,6 +89,9 @@ typedef struct samples_s {
 
     float errorFrequency;
     float errorFrequencyEnergy;
+    
+    timeUs_t hiFreqEvenStartUs;
+
 } samples_t;
 
 static currentSample_t currentSample[XYZ_AXIS_COUNT];
@@ -84,6 +101,54 @@ void qTunePushSample(const flight_dynamics_index_t axis, const float setpoint, c
     currentSample[axis].setpoint = setpoint;
     currentSample[axis].measurement = measurement;
     currentSample[axis].iTerm = iTerm;
+}
+
+static void qTuneEnableState(uint8_t *data, qTuneState_e state) {
+    *data |= state;
+}
+
+static void qTuneDisableState(uint8_t *data, qTuneState_e state) {
+    *data &= ~state;
+}
+
+static bool qTuneState(uint8_t data, qTuneState_e state) {
+    return data & state;
+}
+
+static void hiFrequencyDetector(samples_t * data, timeUs_t currentTimeUs) {
+
+    /*
+     * High frequency oscillation happens only when throttle is not low. This is due to the fact that the motors do not have enough
+     * authority to feed enough energy to the whole system.
+     * If a multitoror oscialltes on high throttle, it will oscillate on low as well
+     * as it will start on high throttle. 
+     */
+    if (rcCommand[THROTTLE] > 1200) {
+
+        if (
+            !qTuneState(data->state, Q_TUNE_STATE_HI_FREQ_START) && 
+            data->errorFrequency >= Q_TUNE_HI_FREQ_THRESHOLD &&
+            currentTimeUs - data->hiFreqEvenStartUs > Q_TUNE_HI_FREQ_EVENT_PERIOD_US    // At least so many us between two events
+        ) {
+            // Osciallation started
+            qTuneEnableState(&data->state, Q_TUNE_STATE_HI_FREQ_START);
+            data->hiFreqEvenStartUs = currentTimeUs;
+        } else if (
+            qTuneState(data->state, Q_TUNE_STATE_HI_FREQ_START) && 
+            currentTimeUs - data->hiFreqEvenStartUs > Q_TUNE_HI_FREQ_EVENT_THRESHOLD_US
+        ) {
+            //Oscillation continues for at least Q_TUNE_HI_FREQ_EVENT_THRESHOLD_US us
+            qTuneEnableState(&data->state, Q_TUNE_STATE_HI_FREQ_OSCILLATION);
+        } else if (data->errorFrequency < Q_TUNE_HI_FREQ_THRESHOLD) {
+            //Oscillation ended
+            qTuneDisableState(&data->state, Q_TUNE_STATE_HI_FREQ_START);
+            qTuneDisableState(&data->state, Q_TUNE_STATE_HI_FREQ_OSCILLATION);
+        }
+    
+    } else {
+        qTuneDisableState(&data->state, Q_TUNE_STATE_HI_FREQ_OSCILLATION);
+        qTuneDisableState(&data->state, Q_TUNE_STATE_HI_FREQ_START);
+    }
 }
 
 static void getSampleFrequency(float *frequency, float *energy, arm_rfft_fast_instance_f32 *structure, float buffer[], const uint16_t bufferLength) {
@@ -111,7 +176,6 @@ static void getSampleFrequency(float *frequency, float *energy, arm_rfft_fast_in
 }
 
 void qTuneProcessTask(timeUs_t currentTimeUs) {
-    UNUSED(currentTimeUs);
 
     static bool initialized = false;
     if (!initialized) {
@@ -119,9 +183,12 @@ void qTuneProcessTask(timeUs_t currentTimeUs) {
 
             arm_rfft_fast_init_f32(&samples[i].errorFft, Q_TUNE_SHORT_BUFFER_LENGTH);
 
+            samples[i].state = 0;
             samples[i].indexShort = 0;
             samples[i].indexLong = 0;
+            samples[i].hiFreqEvenStartUs = 0;
             samples[i].lastExecution = 0;
+
             pt1FilterInit(&samples[i].setpointFilter, Q_TUNE_SETPOINT_LPF_HZ, Q_TUNE_UPDATE_US * 1e-6f);
             pt1FilterInit(&samples[i].measurementFilter, Q_TUNE_MEASUREMENT_LPF_HZ, Q_TUNE_UPDATE_US * 1e-6f);
             pt1FilterInit(&samples[i].pt1ErrorHpfFilter, Q_TUNE_ERROR_HPF_HZ, Q_TUNE_UPDATE_US * 1e-6f);
@@ -188,17 +255,21 @@ void qTuneProcessTask(timeUs_t currentTimeUs) {
 
         memcpy(dataBuffer, axisSample->error, sizeof(axisSample->error));
         getSampleFrequency(&axisSample->errorFrequency, &axisSample->errorFrequencyEnergy, &axisSample->errorFft, dataBuffer, Q_TUNE_SHORT_BUFFER_LENGTH);
+    
+        hiFrequencyDetector(axisSample, currentTimeUs);
+    
     }
 
     // Step 3 - Write blackbox data
     DEBUG_SET(DEBUG_Q_TUNE, 0, samples[FD_ROLL].error[samples[FD_ROLL].indexShort] * 1000.0f);
     DEBUG_SET(DEBUG_Q_TUNE, 1, samples[FD_ROLL].errorRms * 10000.0f);
     DEBUG_SET(DEBUG_Q_TUNE, 2, samples[FD_ROLL].errorStdDev * 10000.0f);
-    DEBUG_SET(DEBUG_Q_TUNE, 3, samples[FD_ROLL].iTermRms * 10000.0f);
-    DEBUG_SET(DEBUG_Q_TUNE, 4, samples[FD_ROLL].iTermStdDev * 10000.0f);
+    DEBUG_SET(DEBUG_Q_TUNE, 3, samples[FD_ROLL].state);
+    // DEBUG_SET(DEBUG_Q_TUNE, 3, samples[FD_ROLL].iTermRms * 10000.0f);
+    // DEBUG_SET(DEBUG_Q_TUNE, 4, samples[FD_ROLL].iTermStdDev * 10000.0f);
     DEBUG_SET(DEBUG_Q_TUNE, 5, samples[FD_ROLL].setpointDerivative * Q_TUNE_UPDATE_RATE_HZ * 1000.0f);
     DEBUG_SET(DEBUG_Q_TUNE, 6, samples[FD_ROLL].errorFrequency);
-    DEBUG_SET(DEBUG_Q_TUNE, 7, samples[FD_ROLL].errorFrequencyEnergy);
+    DEBUG_SET(DEBUG_Q_TUNE, 7, samples[FD_ROLL].errorFrequencyEnergy * 10000.0f);
 
     for (int i = 0; i < XYZ_AXIS_COUNT; i++) {
         samples[i].indexShort = (samples[i].indexShort + 1) % Q_TUNE_SHORT_BUFFER_LENGTH;
