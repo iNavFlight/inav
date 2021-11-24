c7a158aa9d2d258a507ce83ac77752cf04280b87
JuliooCesarMDM <juliomatias123@outlook.com>
2021-11-24 18:45:03 -0300

    fourth tentative to fix compilation error

--- src/test/unit/flight_imu_unittest.cc
+++ src/test/unit/flight_imu_unittest.cc
@@ -28,6 +28,8 @@ extern "C" {
     #include "fc/runtime_config.h"
 
     #include "io/gps.h"
+    
+    #include "fc/config.h"
 
     #include "flight/imu.h"
 }
@@ -137,4 +139,6 @@ void resetHeadingHoldTarget(int16_t heading)
     UNUSED(heading);
 }
 bool isGPSHeadingValid(void) { return true; }
+void readEEPROM(void) {}
+void writeEEPROM(void) {}
 }
