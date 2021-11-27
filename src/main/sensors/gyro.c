d7f008c7b96f50143e24e5d9228ec02b66c7ef91
JuliooCesarMDM <juliomatias123@outlook.com>
2021-11-26 21:04:45 -0300

    verify condition with return

--- src/main/sensors/gyro.c
+++ src/main/sensors/gyro.c
@@ -355,6 +355,15 @@ bool gyroIsCalibrationComplete(void)
 
 STATIC_UNIT_TESTED void performGyroCalibration(gyroDev_t *dev, zeroCalibrationVector_t *gyroCalibration)
 {
+    if (!gyroConfig()->init_gyro_cal_enabled) {
+        gyroCalibration[0].params.state = ZERO_CALIBRATION_DONE; // calibration ended
+        // pass the calibration values
+        dev->gyroZero[X] = gyroConfig()->gyro_zero_cal[X];
+        dev->gyroZero[Y] = gyroConfig()->gyro_zero_cal[Y];
+        dev->gyroZero[Z] = gyroConfig()->gyro_zero_cal[Z];
+        return; // skip gyro calibration and use values ​​read from storage
+    }
+
     fpVector3_t v;
 
     //if (gyroConfig()->init_gyro_cal_enabled) {
