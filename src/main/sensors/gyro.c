b3930f6cdd42786f14c1a29118de39db99005ad8
JuliooCesarMDM <juliomatias123@outlook.com>
2021-11-26 21:26:40 -0300

    test init_gyro_cal_enabled with true verification in if condition

--- src/main/sensors/gyro.c
+++ src/main/sensors/gyro.c
@@ -355,7 +355,7 @@ bool gyroIsCalibrationComplete(void)
 
 STATIC_UNIT_TESTED void performGyroCalibration(gyroDev_t *dev, zeroCalibrationVector_t *gyroCalibration)
 {
-    if (!gyroConfig()->init_gyro_cal_enabled) {
+    if (gyroConfig()->init_gyro_cal_enabled) {
         gyroCalibration[0].params.state = ZERO_CALIBRATION_DONE; // calibration ended
         // pass the calibration values
         dev->gyroZero[X] = gyroConfig()->gyro_zero_cal[X];
@@ -366,40 +366,32 @@ STATIC_UNIT_TESTED void performGyroCalibration(gyroDev_t *dev, zeroCalibrationVe
 
     fpVector3_t v;
 
-    //if (gyroConfig()->init_gyro_cal_enabled) {
-        // Consume gyro reading
-        v.v[X] = dev->gyroADCRaw[X];
-        v.v[Y] = dev->gyroADCRaw[Y];
-        v.v[Z] = dev->gyroADCRaw[Z];
+    // Consume gyro reading
+    v.v[X] = dev->gyroADCRaw[X];
+    v.v[Y] = dev->gyroADCRaw[Y];
+    v.v[Z] = dev->gyroADCRaw[Z];
 
-        zeroCalibrationAddValueV(gyroCalibration, &v);
+    zeroCalibrationAddValueV(gyroCalibration, &v);
 
-        // Check if calibration is complete after this cycle
-        if (zeroCalibrationIsCompleteV(gyroCalibration)) {
-            zeroCalibrationGetZeroV(gyroCalibration, &v);
-            dev->gyroZero[X] = v.v[X];
-            dev->gyroZero[Y] = v.v[Y];
-            dev->gyroZero[Z] = v.v[Z];
+    // Check if calibration is complete after this cycle
+    if (zeroCalibrationIsCompleteV(gyroCalibration)) {
+        zeroCalibrationGetZeroV(gyroCalibration, &v);
+        dev->gyroZero[X] = v.v[X];
+        dev->gyroZero[Y] = v.v[Y];
+        dev->gyroZero[Z] = v.v[Z];
             
-            gyro.getZero[X] = dev->gyroZero[X];
-            gyro.getZero[Y] = dev->gyroZero[Y];
-            gyro.getZero[Z] = dev->gyroZero[Z];
-            gyro.ok_to_save_cal = true;
+        gyro.getZero[X] = dev->gyroZero[X];
+        gyro.getZero[Y] = dev->gyroZero[Y];
+        gyro.getZero[Z] = dev->gyroZero[Z];
+        gyro.ok_to_save_cal = true;
             
-            LOG_D(GYRO, "Gyro calibration complete (%d, %d, %d)", dev->gyroZero[X], dev->gyroZero[Y], dev->gyroZero[Z]);
-            schedulerResetTaskStatistics(TASK_SELF); // so calibration cycles do not pollute tasks statistics
-        } else {
-            dev->gyroZero[X] = 0;
-            dev->gyroZero[Y] = 0;
-            dev->gyroZero[Z] = 0;
-        }
-    /*} else { // skip gyro calibration and use values ​​read from storage
-        gyroCalibration[0].params.state = ZERO_CALIBRATION_DONE; // calibration ended
-        // pass the calibration values
-        dev->gyroZero[X] = gyroConfig()->gyro_zero_cal[X];
-        dev->gyroZero[Y] = gyroConfig()->gyro_zero_cal[Y];
-        dev->gyroZero[Z] = gyroConfig()->gyro_zero_cal[Z];
-    }*/
+        LOG_D(GYRO, "Gyro calibration complete (%d, %d, %d)", dev->gyroZero[X], dev->gyroZero[Y], dev->gyroZero[Z]);
+        schedulerResetTaskStatistics(TASK_SELF); // so calibration cycles do not pollute tasks statistics
+    } else {
+        dev->gyroZero[X] = 0;
+        dev->gyroZero[Y] = 0;
+        dev->gyroZero[Z] = 0;
+    }
 }
 
 /*
