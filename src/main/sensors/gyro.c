9bbd9741f597a7a80701f3e76b158176de444aff
Paweł Spychalski <pspychalski@gmail.com>
2021-12-23 12:41:51 +0100

    Merge pull request #7597 from iNavFlight/dzikuvx-matrix-filter-rework
    Matrix filter rework

--- src/main/sensors/gyro.c
+++ src/main/sensors/gyro.c
@@ -479,8 +479,8 @@ void FAST_CODE NOINLINE gyroFilter()
         if (gyroAnalyseState.filterUpdateExecute) {
             dynamicGyroNotchFiltersUpdate(
                 &dynamicGyroNotchState, 
-                gyroAnalyseState.filterUpdateAxis, 
-                gyroAnalyseState.filterUpdateFrequency
+                gyroAnalyseState.filterUpdateAxis,
+                gyroAnalyseState.centerFrequency[gyroAnalyseState.filterUpdateAxis]
             );
         }
     }
