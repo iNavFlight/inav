2213756368c6fcba752a1b0a23aa20f7d8a1b8cd
ErNis <4658853+erstec@users.noreply.github.com>
2022-04-04 23:24:44 +0300

    Delete install-toolchain.sh

--- install-toolchain.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ ! -d $PWD/tools/gcc-arm-none-eabi-10-2020-q4-major/bin ] ; then
-    curl --retry 10 --retry-max-time 120 -L "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2" | tar xfj - -C $PWD/tools/
-fi
