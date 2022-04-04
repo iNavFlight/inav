d0cae06594cc0fcd96d001607160cfecf67b341c
ErNis <4658853+erstec@users.noreply.github.com>
2022-04-04 22:51:06 +0300

    Project files updated (debug ready)

--- install-toolchain.sh
+++ install-toolchain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ ! -d $PWD/gcc-arm-none-eabi-8-2018-q4-major/bin ] ; then
-    curl --retry 10 --retry-max-time 120 -L "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2" | tar xfj -
+if [ ! -d $PWD/tools/gcc-arm-none-eabi-10-2020-q4-major/bin ] ; then
+    curl --retry 10 --retry-max-time 120 -L "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2" | tar xfj - -C $PWD/tools/
 fi
