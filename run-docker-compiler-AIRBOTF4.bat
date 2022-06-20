ad06f0bc46ebec7a4d6de73a8dc3ef63fa1c8890
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2022-06-20 04:02:13 +0300

    updated batch files

--- run-docker-compiler-AIRBOTF4.bat
+++ run-docker-compiler-AIRBOTF4.bat
@@ -1,7 +1,7 @@
 @rem -rm = automatically clean up the container and remove the file system when the container exits
 @rem -it = starts the container in the interactive mode that allows you to interact with /bin/bash of the container
 @rem -v = link local forder to folder inside machine
-docker run --rm -it -u root -v //d/Github/inav/inav-xplane/inav:/src inav-build AIRBOTF4
+docker run --rm -it -u root -v //d/Github/inav/inav:/src inav-build AIRBOTF4
 
 
 
