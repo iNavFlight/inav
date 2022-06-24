45dc98d16bf3c79c42248edc396aff57484a88e9
MarkTan <tdl0088@163.com>
2022-06-24 13:36:55 +0800

    Update target.h

--- src/main/target/AOCODARCF7DUAL/target.h
+++ src/main/target/AOCODARCF7DUAL/target.h
@@ -27,7 +27,7 @@
 #define BEEPER                  PC13
 #define BEEPER_INVERTED
 
-// *************** SPI1 Gyro & ACC *******************
+// *************** SPI2 Gyro & ACC *******************
 #define USE_SPI
 #define USE_SPI_DEVICE_2
 #define SPI2_SCK_PIN            PB13
@@ -46,11 +46,11 @@
 #define MPU6000_SPI_BUS         BUS_SPI2
 #define MPU6000_EXTI_PIN        PC4
 
-#define USE_IMU_MPU6500
-#define IMU_MPU6500_ALIGN       CW270_DEG
-#define MPU6500_CS_PIN          PB12
-#define MPU6500_SPI_BUS         BUS_SPI2
-#define MPU6500_EXTI_PIN        PC4
+// #define USE_IMU_MPU6500
+// #define IMU_MPU6500_ALIGN       CW270_DEG
+// #define MPU6500_CS_PIN          PB12
+// #define MPU6500_SPI_BUS         BUS_SPI2
+// #define MPU6500_EXTI_PIN        PC4
 
 #define USE_IMU_BMI270
 #define IMU_BMI270_ALIGN        CW180_DEG
@@ -92,7 +92,7 @@
 #define USE_RANGEFINDER
 #define RANGEFINDER_I2C_BUS     BUS_I2C2
 
-// *************** SPI2 OSD ***********************
+// *************** SPI1 OSD ***********************
 #define USE_SPI_DEVICE_1
 #define SPI1_SCK_PIN            PA5
 #define SPI1_MISO_PIN           PA6
