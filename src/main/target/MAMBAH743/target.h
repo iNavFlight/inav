82cab8df78aebfec1899f4b6748ca75de7751fc6
JuliooCesarMDM <juliomatias123@outlook.com>
2022-06-27 20:47:37 -0300

    third attempt to fix the build

--- src/main/target/MAMBAH743/target.h
+++ src/main/target/MAMBAH743/target.h
@@ -18,17 +18,16 @@
 
 #pragma once
 
-#define TARGET_BOARD_IDENTIFIER "H743"
-#define USBD_PRODUCT_STRING     "MATEKH743"
+#define TARGET_BOARD_IDENTIFIER "M743"
+#define USBD_PRODUCT_STRING     "MAMBAH743"
 
 #define USE_TARGET_CONFIG
 
-#define LED0                    PE3
+#define LED0                    PE5
 #define LED1                    PE4
 
-#define BEEPER                  PA15
+#define BEEPER                  PE3
 #define BEEPER_INVERTED
-#define BEEPER_PWM_FREQUENCY    2500
 
 // *************** IMU generic ***********************
 #define USE_DUAL_GYRO
@@ -42,36 +41,25 @@
 #define USE_SPI_DEVICE_1
 #define SPI1_SCK_PIN            PA5
 #define SPI1_MISO_PIN           PA6
-#define SPI1_MOSI_PIN           PD7
+#define SPI1_MOSI_PIN           PA7
+
+// SPI4 is used on the second MPU6000 gyro, we do not use it at the moment
+// #define USE_SPI_DEVICE_4
+// #define SPI4_SCK_PIN            PE12
+// #define SPI4_MISO_PIN           PE13
+// #define SPI4_MOSI_PIN           PE14
 
 #define USE_IMU_MPU6000
+#define IMU_MPU6000_ALIGN       CW180_DEG
+#define MPU6000_SPI_BUS         BUS_SPI1
+#define MPU6000_CS_PIN          PA4
+#define MPU6000_EXTI_PIN        PC4
 
-#define IMU_MPU6000_ALIGN       CW0_DEG_FLIP
-#define MPU6000_SPI_BUS          BUS_SPI1
-#define MPU6000_CS_PIN          PC15
-#define MPU6000_EXTI_PIN        PB2
-
-// *************** SPI4 IMU1  ICM20602 **************
-#define USE_SPI_DEVICE_4
-#define SPI4_SCK_PIN            PE12
-#define SPI4_MISO_PIN           PE13
-#define SPI4_MOSI_PIN           PE14
-
-#define USE_IMU_MPU6500
-
-#define IMU_MPU6500_ALIGN       CW0_DEG_FLIP
-#define MPU6500_SPI_BUS         BUS_SPI4
-#define MPU6500_CS_PIN          PE11
-#define MPU6500_EXTI_PIN        PE15
-
-// *************** SPI4 IMU2 ICM42605 **************
-#define USE_IMU_ICM42605
-
-#define IMU_ICM42605_ALIGN      CW90_DEG_FLIP
-#define ICM42605_SPI_BUS        BUS_SPI4
-#define ICM42605_CS_PIN         PC13
-#define ICM42605_EXTI_PIN       PC14
-
+#define USE_IMU_BMI270
+#define IMU_BMI270_ALIGN        CW180_DEG
+#define BMI270_SPI_BUS          BUS_SPI1
+#define BMI270_CS_PIN           PA4
+#define BMI270_EXTI_PIN         PC4
 
 // *************** SPI2 OSD ***********************
 #define USE_SPI_DEVICE_2
@@ -116,7 +104,7 @@
 #define I2C2_SDA                PB11
 
 #define USE_BARO
-#define BARO_I2C_BUS            BUS_I2C2
+#define BARO_I2C_BUS            BUS_I2C1
 #define USE_BARO_BMP280
 #define USE_BARO_MS5611
 #define USE_BARO_DPS310
@@ -130,10 +118,11 @@
 #define USE_MAG_IST8308
 #define USE_MAG_MAG3110
 #define USE_MAG_LIS3MDL
+#define USE_MAG_VCM5883
 
-#define TEMPERATURE_I2C_BUS     BUS_I2C2
-#define BNO055_I2C_BUS          BUS_I2C2
-#define PITOT_I2C_BUS           BUS_I2C2
+#define TEMPERATURE_I2C_BUS     BUS_I2C1
+#define BNO055_I2C_BUS          BUS_I2C1
+#define PITOT_I2C_BUS           BUS_I2C1
 
 #define USE_RANGEFINDER
 #define RANGEFINDER_I2C_BUS     BUS_I2C1
@@ -154,8 +143,12 @@
 #define UART3_RX_PIN            PD9
 
 #define USE_UART4
-#define UART4_TX_PIN            PB9
-#define UART4_RX_PIN            PB8
+#define UART4_TX_PIN            PD1
+#define UART4_RX_PIN            PD0
+
+#define USE_UART5
+#define UART5_TX_PIN            PC12
+#define UART5_RX_PIN            PD2
 
 #define USE_UART6
 #define UART6_TX_PIN            PC6
@@ -169,34 +162,20 @@
 #define UART8_TX_PIN            PE1
 #define UART8_RX_PIN            PE0
 
-#define USE_SOFTSERIAL1
-#define SOFTSERIAL_1_TX_PIN      PC6  //TX6 pad
-#define SOFTSERIAL_1_RX_PIN      PC6  //TX6 pad
-
 #define SERIAL_PORT_COUNT       9
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_SBUS
 #define SERIALRX_UART           SERIAL_PORT_USART6
 
-// *************** SDIO SD BLACKBOX*******************
-#define USE_SDCARD
-#define USE_SDCARD_SDIO
-#define SDCARD_SDIO_DEVICE      SDIODEV_1
-#define SDCARD_SDIO_4BIT
-
-#define ENABLE_BLACKBOX_LOGGING_ON_SDCARD_BY_DEFAULT
-
 // *************** ADC *****************************
 #define USE_ADC
-#define ADC_INSTANCE                ADC3
+#define ADC_INSTANCE                ADC1
 
-#define ADC_CHANNEL_1_PIN           PC0  //ADC123 VBAT1
-#define ADC_CHANNEL_2_PIN           PC1  //ADC123 CURR1
-#define ADC_CHANNEL_3_PIN           PC5  //ADC12  RSSI
-#define ADC_CHANNEL_4_PIN           PC4  //ADC12  AirS
-#define ADC_CHANNEL_5_PIN           PA4  //ADC12  VB2
-#define ADC_CHANNEL_6_PIN           PA7  //ADC12  CU2
+#define ADC_CHANNEL_1_PIN           PC1
+#define ADC_CHANNEL_2_PIN           PC3
+#define ADC_CHANNEL_3_PIN           PC2
+#define ADC_CHANNEL_4_PIN           PC0
 
 #define VBAT_ADC_CHANNEL            ADC_CHN_1
 #define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
@@ -206,14 +185,13 @@
 // *************** PINIO ***************************
 #define USE_PINIO
 #define USE_PINIOBOX
-#define PINIO1_PIN                  PD10  // VTX power switcher
-#define PINIO2_PIN                  PD11  // 2xCamera switcher
+#define PINIO1_PIN                  PC5
 
 // *************** LEDSTRIP ************************
 #define USE_LED_STRIP
 #define WS2811_PIN                  PA8
 
-#define DEFAULT_FEATURES            (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TX_PROF_SEL | FEATURE_BLACKBOX)
+#define DEFAULT_FEATURES            (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TX_PROF_SEL)
 #define CURRENT_METER_SCALE         250
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
@@ -224,6 +202,7 @@
 #define TARGET_IO_PORTD 0xffff
 #define TARGET_IO_PORTE 0xffff
 
-#define MAX_PWM_OUTPUT_PORTS        15
+#define MAX_PWM_OUTPUT_PORTS        8
 #define USE_DSHOT
 #define USE_ESC_SENSOR
+
