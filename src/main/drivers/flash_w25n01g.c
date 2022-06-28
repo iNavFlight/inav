348e087b0ccd1bd5a077a4398fd6eb796ee7c741
JuliooCesarMDM <juliomatias123@outlook.com>
2022-06-28 20:46:36 -0300

    fix machine cycle lockup

--- src/main/drivers/flash_w25n01g.c
+++ src/main/drivers/flash_w25n01g.c
@@ -251,9 +251,9 @@ bool w25n01g_detect(uint32_t chipID)
     geometry.sectorSize = geometry.pagesPerSector * geometry.pageSize;
     geometry.totalSize = geometry.sectorSize * geometry.sectors;
     
-    flashPartitionSet(FLASH_PARTITION_TYPE_BADBLOCK_MANAGEMENT,
+    /*flashPartitionSet(FLASH_PARTITION_TYPE_BADBLOCK_MANAGEMENT,
             W25N01G_BB_MANAGEMENT_START_BLOCK,
-            W25N01G_BB_MANAGEMENT_START_BLOCK + W25N01G_BB_MANAGEMENT_BLOCKS - 1);
+            W25N01G_BB_MANAGEMENT_START_BLOCK + W25N01G_BB_MANAGEMENT_BLOCKS - 1);*/
 
     couldBeBusy = true; // Just for luck we'll assume the chip could be busy even though it isn't specced to be
 
