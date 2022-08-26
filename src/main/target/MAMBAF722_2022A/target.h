3e1d1511dfda0dfb75cb84e8e8004797b2ad7c3a
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2022-08-26 09:35:06 +0200

    MAMBAF722_2022B target

--- src/main/target/MAMBAF722_2022A/target.h
+++ src/main/target/MAMBAF722_2022A/target.h
@@ -54,6 +54,16 @@
 #define BMI270_CS_PIN                   SPI1_NSS_PIN
 #define BMI270_EXTI_PIN                 GYRO_INT_EXTI
 
+#ifdef MAMBAF722_2022B
+
+#define USE_IMU_ICM42605
+#define IMU_ICM42605_ALIGN      CW270_DEG
+#define ICM42605_SPI_BUS        BUS_SPI1
+#define ICM42605_CS_PIN         SPI1_NSS_PIN
+#define ICM42605_EXTI_PIN       GYRO_INT_EXTI
+
+#endif
+
 #define USE_I2C
 
 #define USE_I2C_DEVICE_1
