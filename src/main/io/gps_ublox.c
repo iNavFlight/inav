0078a5b55077b70fcacaa975a8152fbb4f8aa92c
Jonathan Hudson <jh+github@daria.co.uk>
2022-09-26 16:04:31 +0100

    use protocol specific base timeout

--- src/main/io/gps_ublox.c
+++ src/main/io/gps_ublox.c
@@ -1047,7 +1047,7 @@ STATIC_PROTOTHREAD(gpsProtocolStateThread)
     }
 
     // GPS setup done, reset timeout
-    gpsSetProtocolTimeout(GPS_TIMEOUT);
+    gpsSetProtocolTimeout(gpsState.baseTimeoutMs);
 
     // GPS is ready - execute the gpsProcessNewSolutionData() based on gpsProtocolReceiverThread semaphore
     while (1) {
