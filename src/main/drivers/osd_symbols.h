e353b6ea7e666028b09472b9aa9734ad0e5a379d
Darren Lines <darren@darrenlines.uk>
2022-11-26 18:04:21 +0000

    Add symbols for remaining time/distance
    Fixes #636
    
    Adds symbols for remaining flight distance and flight time OSD elements. These replace the current fly time and total distance symbols, which are not specific.
    
    I have added temporary symbols which can be improved.

--- src/main/drivers/osd_symbols.h
+++ src/main/drivers/osd_symbols.h
@@ -171,6 +171,8 @@
 #define SYM_AH_V_FT_1               0xD7  // 215 mAh/v-ft right
 #define SYM_AH_V_M_0                0xD8  // 216 mAh/v-m left
 #define SYM_AH_V_M_1                0xD9  // 217 mAh/v-m right
+#define SYM_FLIGHT_MINS_REMAINING   0xDA  // 216 Flight time (mins) remaining
+#define SYM_FLIGHT_HOURS_REMAINING  0xDB  // 217 Flight time (hours) remaining
 
 #define SYM_LOGO_START              0x101 // 257 to 280, INAV logo
 #define SYM_LOGO_WIDTH              6
@@ -220,6 +222,7 @@
 
 #define SYM_HOME_DIST 	            0x165 // 357 DIST
 #define SYM_AH_CH_CENTER            0x166 // 358 Crossair center
+#define SYM_FLIGHT_DIST_REMAINING   0x167 // 359 Flight distance reminaing
 
 #define SYM_AH_CH_TYPE3             0x190 // 400 to 402, crosshair 3
 #define SYM_AH_CH_TYPE4             0x193 // 403 to 405, crosshair 4
