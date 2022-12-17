a83757627edae9513a62604bdf65fe0b51e38518
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2022-12-17 13:12:03 +0100

    remove blank line

--- src/main/io/displayport_msp_bf_compat.h
+++ src/main/io/displayport_msp_bf_compat.h
@@ -27,7 +27,6 @@
 #ifndef DISABLE_MSP_BF_COMPAT
 #include "osd.h"
 uint8_t getBfCharacter(uint8_t ch, uint8_t page);
-
 #define isBfCompatible(osdConfigPtr) (osdConfigPtr->video_system == VIDEO_SYSTEM_BFCOMPAT)
 #else
 #define getBfCharacter(x, page) (x)
