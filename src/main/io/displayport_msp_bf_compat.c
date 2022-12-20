d5987f923a02b438955c2a360b3fef248e0d841e
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2022-12-20 19:30:35 +0100

    Fix arrow direction, hopefully I haven't missed any.

--- src/main/io/displayport_msp_bf_compat.c
+++ src/main/io/displayport_msp_bf_compat.c
@@ -483,49 +483,49 @@ uint8_t getBfCharacter(uint8_t ch, uint8_t page)
         return BF_SYM_ARROW_NORTH;
 
     case SYM_ARROW_2:
-        return BF_SYM_ARROW_2;
+        return BF_SYM_ARROW_8;
 
     case SYM_ARROW_3:
-        return BF_SYM_ARROW_3;
+        return BF_SYM_ARROW_7;
 
     case SYM_ARROW_4:
-        return BF_SYM_ARROW_4;
+        return BF_SYM_ARROW_6;
 
     case SYM_ARROW_RIGHT:
         return BF_SYM_ARROW_EAST;
 
     case SYM_ARROW_6:
-        return BF_SYM_ARROW_6;
+        return BF_SYM_ARROW_4;
 
     case SYM_ARROW_7:
-        return BF_SYM_ARROW_7;
+        return BF_SYM_ARROW_3;
 
     case SYM_ARROW_8:
-        return BF_SYM_ARROW_8;
+        return BF_SYM_ARROW_2;
 
     case SYM_ARROW_DOWN:
         return BF_SYM_ARROW_SOUTH;
 
     case SYM_ARROW_10:
-        return BF_SYM_ARROW_10;
+        return BF_SYM_ARROW_16;
 
     case SYM_ARROW_11:
-        return BF_SYM_ARROW_11;
+        return BF_SYM_ARROW_15;
 
     case SYM_ARROW_12:
-        return BF_SYM_ARROW_12;
+        return BF_SYM_ARROW_14;
 
     case SYM_ARROW_LEFT:
         return BF_SYM_ARROW_WEST;
 
     case SYM_ARROW_14:
-        return BF_SYM_ARROW_14;
+        return BF_SYM_ARROW_12;
 
     case SYM_ARROW_15:
-        return BF_SYM_ARROW_15;
+        return BF_SYM_ARROW_11;
 
     case SYM_ARROW_16:
-        return BF_SYM_ARROW_16;
+        return BF_SYM_ARROW_10;
 
     case SYM_AH_H_START:
         return BF_SYM_AH_BAR9_0;
