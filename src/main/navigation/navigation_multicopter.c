3560bb57ea8c1bec36c59b5cb52806add5d4f1bb
breadoven <56191411+breadoven@users.noreply.github.com>
2023-01-24 14:21:13 +0000

    landing tweaks

--- src/main/navigation/navigation_multicopter.c
+++ src/main/navigation/navigation_multicopter.c
@@ -798,7 +798,7 @@ bool isMulticopterLandingDetected(void)
     DEBUG_SET(DEBUG_LANDING, 5, possibleLandingDetected);
 
     if (possibleLandingDetected) {
-        timeUs_t safetyTimeDelay = MS2US(2000 + navConfig()->general.auto_disarm_delay);  // check conditions stable for 2s + optional extra delay
+        timeUs_t safetyTimeDelay = MS2US(1000 + navConfig()->general.auto_disarm_delay);  // check conditions stable for 1s + optional extra delay
         return (currentTimeUs - landingDetectorStartedAt > safetyTimeDelay);
     } else {
         landingDetectorStartedAt = currentTimeUs;
