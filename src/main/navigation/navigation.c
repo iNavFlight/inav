88cb0971650887e16332c4432c67ce9de15d9998
Filipp Ozinov <filipp@bakanov.su>
2023-01-24 18:02:05 +0400

    Fixed default value

--- src/main/navigation/navigation.c
+++ src/main/navigation/navigation.c
@@ -207,7 +207,7 @@ PG_RESET_TEMPLATE(navConfig_t, navConfig,
         .launch_max_angle = SETTING_NAV_FW_LAUNCH_MAX_ANGLE_DEFAULT,            // 45 deg
         .launch_manual_throttle = SETTING_NAV_FW_LAUNCH_MANUAL_THROTTLE_DEFAULT,// OFF
         .launch_abort_deadband = SETTING_NAV_FW_LAUNCH_ABORT_DEADBAND_DEFAULT,  // 100 us
-        .launch_vtx_lowpower = SETTING_NAV_FW_LAUNCH_VTX_LOWPOWER,              // false
+        .launch_vtx_lowpower = SETTING_NAV_FW_LAUNCH_VTX_LOWPOWER_DEFAULT,      // false
 
         .cruise_yaw_rate  = SETTING_NAV_FW_CRUISE_YAW_RATE_DEFAULT,             // 20dps
         .allow_manual_thr_increase = SETTING_NAV_FW_ALLOW_MANUAL_THR_INCREASE_DEFAULT,
