18c1f07b728ddc435aa11ff9d95d19e5652f4a6e
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2023-03-02 01:12:38 +0100

    Add ICM42688 to Betafpv F722 #8841

--- src/main/target/BETAFPVF722/target.h
+++ src/main/target/BETAFPVF722/target.h
@@ -40,6 +40,11 @@
 #define USE_IMU_MPU6000
 #define IMU_MPU6000_ALIGN       CW180_DEG
 
+#define USE_IMU_ICM42605
+#define IMU_ICM42605_ALIGN      CW180_DEG
+#define ICM42605_SPI_BUS        BUS_SPI1
+#define ICM42605_CS_PIN         PA4
+
 // *************** I2C/Baro/Mag *********************
 #define USE_I2C
 
