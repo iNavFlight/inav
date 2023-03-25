227311f1fd8f20eedf15781006495a288e30a28e
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2023-03-25 12:59:04 +0100

    Pull all servos to min when disarmed if thr is mixed in

--- src/main/flight/servos.c
+++ src/main/flight/servos.c
@@ -360,6 +360,20 @@ void servoMixer(float dT)
         servo[target] += ((int32_t)inputLimited * currentServoMixer[i].rate) / 100;
     }
 
+    /*
+     * When not armed, apply servo low position to all outputs that include a throttle or stabilizet throttle in the mix
+     */
+    if (!ARMING_FLAG(ARMED)) {
+        for (int i = 0; i < servoRuleCount; i++) {
+            const uint8_t target = currentServoMixer[i].targetChannel;
+            const uint8_t from = currentServoMixer[i].inputSource;
+
+            if (from == INPUT_STABILIZED_THROTTLE || from == INPUT_RC_THROTTLE) {
+                servo[target] = motorConfig()->mincommand;
+            }
+        }
+    }
+
     for (int i = 0; i < MAX_SUPPORTED_SERVOS; i++) {
 
         /*
