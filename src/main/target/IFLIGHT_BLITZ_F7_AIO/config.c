664e35bde69381dcf180450189878a7320383f96
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2023-04-13 18:30:07 +0200

    Set onboard led to gree + warnings by default

--- src/main/target/IFLIGHT_BLITZ_F7_AIO/config.c
+++ src/main/target/IFLIGHT_BLITZ_F7_AIO/config.c
@@ -4,6 +4,7 @@
 #include <platform.h>
 
 #include "io/serial.h"
+#include "io/ledstrip.h"
 #include "sensors/boardalignment.h"
 
 void targetConfiguration(void)
@@ -12,4 +13,6 @@ void targetConfiguration(void)
     serialConfigMutable()->portConfigs[findSerialPortIndexByIdentifier(SERIAL_PORT_USART1)].functionMask = FUNCTION_MSP_OSD;
 
     boardAlignmentMutable()->yawDeciDegrees = 1350;
+
+    DEFINE_LED(ledStripConfigMutable()->ledConfigs, 0, 0, COLOR_GREEN, 0, LED_FUNCTION_ARM_STATE, LED_FLAG_OVERLAY(LED_OVERLAY_WARNING), 0);
 }
