84ff2f1415e0fadcc67c73dcf93f3462d931d6bc
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2023-04-14 11:33:49 +0200

    Fomatting and correction to max pwm

--- src/main/target/IFLIGHT_BLITZ_F7_AIO/target.h
+++ src/main/target/IFLIGHT_BLITZ_F7_AIO/target.h
@@ -181,6 +181,6 @@
 #define TARGET_IO_PORTD         0xffff
 #define TARGET_IO_PORTE         0xffff
 
-#define MAX_PWM_OUTPUT_PORTS    8
+#define MAX_PWM_OUTPUT_PORTS    10
 #define USE_DSHOT
 #define USE_ESC_SENSOR
