19d0992930219681480a5834ff8269b93a12a33e
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2023-04-28 17:27:18 +0200

    fixed pitot sensor

--- src/main/sensors/pitotmeter.c
+++ src/main/sensors/pitotmeter.c
@@ -208,17 +208,14 @@ STATIC_PROTOTHREAD(pitotThread)
 
         pitot.dev.calculate(&pitot.dev, &pitotPressureTmp, NULL);
 #ifdef USE_SIMULATOR
-        float airSpeed;
         if (SIMULATOR_HAS_OPTION(HITL_AIRSPEED)) {
-             airSpeed = simulatorData.airSpeed;
-#if defined(USE_PITOT_FAKE)
-        } else if (pitotmeterConfig()->pitot_hardware == PITOT_FAKE) { 
-        	airSpeed = fakePitotGetAirspeed();
-#endif
-    	} else {
-            airSpeed = 0;
+            pitotPressureTmp = sq(simulatorData.airSpeed) * SSL_AIR_DENSITY / 20000.0f + SSL_AIR_PRESSURE;     
         }
-        pitotPressureTmp = sq(airSpeed) * SSL_AIR_DENSITY / 20000.0f + SSL_AIR_PRESSURE;     
+#endif
+#if defined(USE_PITOT_FAKE)
+        if (pitotmeterConfig()->pitot_hardware == PITOT_FAKE) { 
+            pitotPressureTmp = sq(fakePitotGetAirspeed()) * SSL_AIR_DENSITY / 20000.0f + SSL_AIR_PRESSURE;     
+    	} 
 #endif
         ptYield();
 
@@ -246,12 +243,11 @@ STATIC_PROTOTHREAD(pitotThread)
 #ifdef USE_SIMULATOR
         if (SIMULATOR_HAS_OPTION(HITL_AIRSPEED)) {
             pitot.airSpeed = simulatorData.airSpeed;
-#if defined(USE_PITOT_FAKE)
-        } else if (pitotmeterConfig()->pitot_hardware == PITOT_FAKE) { 
-            pitot.airSpeed = fakePitotGetAirspeed();
+        }
 #endif
-        } else {
-            pitot.airSpeed = 0;
+#if defined(USE_PITOT_FAKE)
+        if (pitotmeterConfig()->pitot_hardware == PITOT_FAKE) { 
+            pitot.airSpeed = fakePitotGetAirspeed();
         }
 #endif
     }
