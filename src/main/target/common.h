45a71edbaff6720a2e2fe7986ccc01534747c40b
Daniel Yavorovych <daniel@dysnix.com>
2023-05-21 14:40:06 +0300

    recover

--- src/main/target/common.h
+++ src/main/target/common.h
@@ -155,7 +155,7 @@
 #define USE_TELEMETRY_JETIEXBUS
 // These are rather exotic serial protocols
 #define USE_RX_MSP
-#define USE_MSP_RC_OVERRIDE
+//#define USE_MSP_RC_OVERRIDE
 #define USE_SERIALRX_CRSF
 #define USE_SERIAL_PASSTHROUGH
 #define NAV_MAX_WAYPOINTS       120
