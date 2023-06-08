f984f055192f566a05e3c0c11771cfaa4c13b98a
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2023-06-09 00:33:40 +0200

    small changes to unit test

--- src/test/unit/gps_ublox_unittest.cc
+++ src/test/unit/gps_ublox_unittest.cc
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <stdio.h>
 
 #include "io/gps_ublox_utils.h"
 
@@ -10,11 +11,23 @@ TEST(GPSUbloxTest, TestUbloxCfgFillBytes)
 {
     ubx_config_data8_t cfg = {};
     ubx_config_data8_payload_t kvPairs[] = {
-        { 0x10, 0x1},
+        { 0x04, 0x20},
         { 0x42, 0x69}
     };
 
     ubloxCfgFillBytes(&cfg, kvPairs, 2);
+
+    printf("%02x %02x %02x %02x %04x\n", cfg.header.preamble1, cfg.header.preamble2, cfg.header.msg_class, cfg.header.msg_id, cfg.header.length);
+
+    printf("%02x %02x %02x\n", cfg.configHeader.version, cfg.configHeader.layers, cfg.configHeader.reserved);
+
+    for(int i =0; i < 2; ++i) {
+        printf("%08x %02x\n", cfg.data.payload[i].key, cfg.data.payload[i].value);
+    }
+
+    uint8_t *chksums = (uint8_t *)&cfg.data.payload[2];
+
+    printf("%02x %02x\n", chksums[0], chksums[1]);
 
     // osdFormatCentiNumber(buf, 12345, 1, 2, 3, 7);
     // std::cout << "'" << buf << "'" << std::endl;
