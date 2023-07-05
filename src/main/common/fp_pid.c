3fa823d05be6e13770e6a1000d1db3d007aa3832
breadoven <56191411+breadoven@users.noreply.github.com>
2023-07-05 10:48:01 +0100

    Update fp_pid.c

--- src/main/common/fp_pid.c
+++ src/main/common/fp_pid.c
@@ -40,13 +40,13 @@ float navPidApply3(
     const float dt,
     const float outMin,
     const float outMax,
-    const pidControllerFlags_e pidFlags,
     const float gainScaler,
-    const float dTermScaler
+    const float dTermScaler,
+    pidControllerFlags_e pidFlags
 ) {
     float newProportional, newDerivative, newFeedForward;
     float error = 0.0f;
-    
+
     if (pid->errorLpfHz > 0.0f) {
         error = pt1FilterApply4(&pid->error_filter_state, setpoint - measurement, pid->errorLpfHz, dt);
     } else {
@@ -80,6 +80,11 @@ float navPidApply3(
 
     newDerivative = newDerivative * gainScaler * dTermScaler;
 
+    // zero integral if proportional saturated
+    if (newProportional > outMax || newProportional < outMin) {
+        pidFlags |= PID_ZERO_INTEGRATOR;
+    }
+
     if (pidFlags & PID_ZERO_INTEGRATOR) {
         pid->integrator = 0.0f;
     }
@@ -102,7 +107,7 @@ float navPidApply3(
     /* Update I-term */
     if (
         !(pidFlags & PID_ZERO_INTEGRATOR) &&
-        !(pidFlags & PID_FREEZE_INTEGRATOR) 
+        !(pidFlags & PID_FREEZE_INTEGRATOR)
     ) {
         const float newIntegrator = pid->integrator + (error * pid->param.kI * gainScaler * dt) + ((outValConstrained - outVal) * pid->param.kT * dt);
 
@@ -116,10 +121,10 @@ float navPidApply3(
             pid->integrator = newIntegrator;
         }
     }
-    
+
     if (pidFlags & PID_LIMIT_INTEGRATOR) {
         pid->integrator = constrainf(pid->integrator, outMin, outMax);
-    } 
+    }
 
     return outValConstrained;
 }
