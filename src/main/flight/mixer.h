2c0e468ba445a6601f633eeb01924d8644b806ad
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2023-09-18 17:51:07 +0200

    Drop output_mode setting

--- src/main/flight/mixer.h
+++ src/main/flight/mixer.h
@@ -77,7 +77,6 @@ typedef struct mixerConfig_s {
     uint8_t platformType;
     bool hasFlaps;
     int16_t appliedMixerPreset;
-    uint8_t outputMode;
 } mixerConfig_t;
 
 PG_DECLARE(mixerConfig_t, mixerConfig);
