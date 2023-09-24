589abe0651d8b8548ed941a3f384beecc4aaad53
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2023-09-25 00:39:55 +0200

    Update target.c

--- src/main/target/IFLIGHTF7_TWING/target.c
+++ src/main/target/IFLIGHTF7_TWING/target.c
@@ -37,11 +37,11 @@ timerHardware_t timerHardware[] = {
     DEF_TIM(TIM8, CH4, PC9, TIM_USE_OUTPUT_AUTO, 0, 0),
     DEF_TIM(TIM8, CH2, PC7, TIM_USE_OUTPUT_AUTO, 0, 0),
 
-    DEF_TIM(TIM4, CH1, PB6, TIM_USE_OUTPUT_AUTO, 0, 0),
-    DEF_TIM(TIM4, CH2, PB7, TIM_USE_OUTPUT_AUTO, 0, 0),
+    DEF_TIM(TIM4, CH1, PB6, TIM_USE_OUTPUT_AUTO, 0, 0), // used to be fw motor
+    DEF_TIM(TIM4, CH2, PB7, TIM_USE_OUTPUT_AUTO, 0, 0), // used to be fw motor
 
-    DEF_TIM(TIM3, CH4, PB1, TIM_USE_OUTPUT_AUTO, 0, 0), // used to be fw motor
-    DEF_TIM(TIM3, CH3, PB0, TIM_USE_OUTPUT_AUTO, 0, 0), // used to be fw motor
+    DEF_TIM(TIM3, CH4, PB1, TIM_USE_OUTPUT_AUTO, 0, 0),
+    DEF_TIM(TIM3, CH3, PB0, TIM_USE_OUTPUT_AUTO, 0, 0),
 
     DEF_TIM(TIM2, CH2, PA1, TIM_USE_LED,         0, 0),
 };
