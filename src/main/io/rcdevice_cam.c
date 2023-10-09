37eb95e2d87eef9510052bd9e5d330c55471f8c5
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2023-10-09 15:16:28 +0200

    fixed compilation error

--- src/main/io/rcdevice_cam.c
+++ src/main/io/rcdevice_cam.c
@@ -116,6 +116,7 @@ static void rcdeviceCameraControlProcess(void)
                 runcamDeviceSimulateCameraButton(camDevice, behavior);
                 switchStates[switchIndex].isActivated = true;
             }
+            UNUSED(behavior1);
 #ifndef UNIT_TEST
 #ifdef USE_LED_STRIP
             else if ((behavior1 != RCDEVICE_PROTOCOL_CAM_CTRL_UNKNOWN_CAMERA_OPERATION) && osdJoystickEnabled()) {
