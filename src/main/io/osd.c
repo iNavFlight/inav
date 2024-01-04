b35cb0eefbe23db07075ca53c8dfa79d446bc92c
Darren Lines <darren@darrenlines.uk>
2024-01-04 21:23:15 +0000

    Missed #if defined(USE_ESC_SENSOR)

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -4344,7 +4344,7 @@ static void osdUpdateStats(void)
         if (stats.max_sats < gpsSol.numSat)
             stats.max_sats = gpsSol.numSat;
     }
-
+#if defined(USE_ESC_SENSOR)
     if (STATE(ESC_SENSOR_ENABLED)) {
         escSensorData_t * escSensor = escSensorGetData();
         bool escTemperatureValid = escSensor && escSensor->dataAge <= ESC_DATA_MAX_AGE;
@@ -4357,6 +4357,7 @@ static void osdUpdateStats(void)
                 stats.max_esc_temp = escSensor->temperature;
         }
     }
+#endif
 
     value = getBatteryVoltage();
     if (stats.min_voltage > value)
