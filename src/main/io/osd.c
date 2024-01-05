31cbfde18013ba7e629b59c14715d7e225ad14aa
Darren Lines <darren@darrenlines.uk>
2024-01-05 21:54:57 +0000

    Fix Mac SITL test

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -28,6 +28,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <inttypes.h>
 
 #include "platform.h"
 
@@ -1988,7 +1989,7 @@ static bool osdDrawSingleElement(uint8_t item)
                 } else {
                     int32_t logNumber = blackboxGetLogNumber();
                     if (logNumber >= 0) {
-                        tfp_sprintf(buff, "%c%05ld", SYM_BLACKBOX, logNumber);
+                        tfp_sprintf(buff, "%c%05" PRId32, SYM_BLACKBOX, logNumber);
                     } else {
                         tfp_sprintf(buff, "%c", SYM_BLACKBOX);
                     }
@@ -4786,7 +4787,7 @@ static void osdShowStats(bool isSinglePageStatsCompatible, uint8_t page)
         if (feature(FEATURE_BLACKBOX)) {
             int32_t logNumber = blackboxGetLogNumber();
             if (logNumber >= 0)
-                tfp_sprintf(buff, "%s %c%05ld ", buff, SYM_BLACKBOX, logNumber);
+                tfp_sprintf(buff, "%s %c%05 " PRId32, buff, SYM_BLACKBOX, logNumber);
             else
                 tfp_sprintf(buff, "%s %c ", buff, SYM_BLACKBOX);
         }
