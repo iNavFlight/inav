81bd236577cd960544d7b7ede5d1ad1273b420a0
Darren Lines <darren@darrenlines.uk>
2024-02-11 08:51:24 +0000

    Revert "Delay save settings to prevent ESC lockup on emergency rearm"

--- src/main/fc/config.c
+++ src/main/fc/config.c
@@ -68,7 +68,6 @@
 
 #include "fc/config.h"
 #include "fc/controlrate_profile.h"
-#include "fc/fc_core.h"
 #include "fc/rc_adjustments.h"
 #include "fc/rc_controls.h"
 #include "fc/rc_curves.h"
@@ -397,30 +396,16 @@ void saveConfig(void)
     }
 }
 
-void processDelayedSave(bool readyToSave)
+void processDelayedSave(void)
 {
     if (saveState == SAVESTATE_SAVEANDNOTIFY) {
-         if (emergInflightRearmEnabled() || !readyToSave) {
-            // Do not process save if we are potentially still flying. Once armed, this function will not be called until the next disarm.
-#ifdef USE_OSD
-    osdSaveWaitingProcess();
-#endif 
-        } else {
-#ifdef USE_OSD
-    osdStartedSaveProcess();
-#endif
-            if (readyToSave) {
-                processSaveConfigAndNotify();
-                saveState = SAVESTATE_NONE;
-            }
-        }
+        processSaveConfigAndNotify();
+        saveState = SAVESTATE_NONE;
     } else if (saveState == SAVESTATE_SAVEONLY) {
-        if (readyToSave) {
-            suspendRxSignal();
-            writeEEPROM();
-            resumeRxSignal();
-            saveState = SAVESTATE_NONE;
-        }
+        suspendRxSignal();
+        writeEEPROM();
+        resumeRxSignal();
+        saveState = SAVESTATE_NONE;
     }
 }
 
--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -155,16 +155,8 @@
 #define OSD_MIN_FONT_VERSION 3
 
 static timeMs_t linearDescentMessageMs  = 0;
-
-typedef enum {
-    OSD_SAVE_MESSAGE_NONE,
-    OSD_SAVE_MESSAGE_WAITING,
-    OSD_SAVE_MESSAGE_SAVING,
-    OSD_SAVE_MESSAGE_SAVED
-} osd_saveMessage_e;
-
 static timeMs_t notify_settings_saved   = 0;
-static uint8_t  savingSettings          = OSD_SAVE_MESSAGE_NONE;
+static bool     savingSettings          = false;
 
 static unsigned currentLayout = 0;
 static int layoutOverride = -1;
@@ -220,23 +212,16 @@ static bool osdDisplayHasCanvas;
 PG_REGISTER_WITH_RESET_TEMPLATE(osdConfig_t, osdConfig, PG_OSD_CONFIG, 9);
 PG_REGISTER_WITH_RESET_FN(osdLayoutsConfig_t, osdLayoutsConfig, PG_OSD_LAYOUTS_CONFIG, 1);
 
-void osdSaveProcessAborted(void) {
-    notify_settings_saved = 0;
-    savingSettings = OSD_SAVE_MESSAGE_NONE;
-}
-
-void osdSaveWaitingProcess(void) {
-    savingSettings = OSD_SAVE_MESSAGE_WAITING;
-}
-
 void osdStartedSaveProcess(void) {
-    savingSettings = OSD_SAVE_MESSAGE_SAVING;
+    savingSettings = true;
 }
 
 void osdShowEEPROMSavedNotification(void) {
-    savingSettings = OSD_SAVE_MESSAGE_SAVED;
+    savingSettings = false;
     notify_settings_saved = millis() + 5000;
 }
+
+
 
 bool osdDisplayIsPAL(void)
 {
@@ -4549,15 +4534,12 @@ static void osdShowStats(bool isSinglePageStatsCompatible, uint8_t page)
         displayWrite(osdDisplayPort, statValuesX + multiValueLengthOffset, top++, buff);
     }
 
-    if (savingSettings == OSD_SAVE_MESSAGE_SAVING) {
+    if (savingSettings == true) {
         displayWrite(osdDisplayPort, statNameX, top++, OSD_MESSAGE_STR(OSD_MSG_SAVING_SETTNGS));
-    } else if (savingSettings == OSD_SAVE_MESSAGE_WAITING) {
-        displayWrite(osdDisplayPort, statNameX, top++, OSD_MESSAGE_STR(OSD_MSG_WAITING_TO_SAVE));
     } else if (notify_settings_saved > 0) {
         if (millis() > notify_settings_saved) {
             notify_settings_saved = 0;
-            savingSettings = OSD_SAVE_MESSAGE_NONE;
-        } else if (savingSettings == OSD_SAVE_MESSAGE_SAVED) {
+        } else {
             displayWrite(osdDisplayPort, statNameX, top++, OSD_MESSAGE_STR(OSD_MSG_SETTINGS_SAVED));
         }
     }
@@ -5283,15 +5265,12 @@ textAttributes_t osdGetSystemMessage(char *buff, size_t buff_size, bool isCenter
 
         /* Messages that are shown regardless of Arming state */
 
-        if (savingSettings == OSD_SAVE_MESSAGE_SAVING) {
+        if (savingSettings == true) {
            messages[messageCount++] = OSD_MESSAGE_STR(OSD_MSG_SAVING_SETTNGS);
-        } else if (savingSettings == OSD_SAVE_MESSAGE_WAITING) {
-            messages[messageCount++] = OSD_MESSAGE_STR(OSD_MSG_WAITING_TO_SAVE);
         } else if (notify_settings_saved > 0) {
             if (millis() > notify_settings_saved) {
                 notify_settings_saved = 0;
-                savingSettings = OSD_SAVE_MESSAGE_NONE;
-            } else if (savingSettings == OSD_SAVE_MESSAGE_SAVED) {
+            } else {
                 messages[messageCount++] = OSD_MESSAGE_STR(OSD_MSG_SETTINGS_SAVED);
             }
         }
