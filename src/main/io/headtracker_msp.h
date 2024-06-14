29a2d6935bdd1f1910f1d2f3a6292f3697d54369
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2024-06-14 11:13:28 +0200

    Add version header, in case we want to expand it in the future

--- src/main/io/headtracker_msp.h
+++ src/main/io/headtracker_msp.h
@@ -28,6 +28,7 @@
 #include "drivers/headtracker_common.h"
 
 typedef struct headtrackerMspMessage_s {
+    uint8_t version;	    // 0
     int16_t pan;            // -2048~2047. Scale is min/max angle for gimbal
     int16_t tilt;           // -2048~2047. Scale is min/max angle for gimbal
     int16_t roll;           // -2048~2047. Scale is min/max angle for gimbal
