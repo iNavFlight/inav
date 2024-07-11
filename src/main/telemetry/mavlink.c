44d99f3dd415064298a2a009f22d60e7e9193d65
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2024-07-11 15:48:39 +0200

    improve half duplex checks

--- src/main/telemetry/mavlink.c
+++ src/main/telemetry/mavlink.c
@@ -1207,6 +1207,11 @@ static bool processMAVLinkIncomingTelemetry(void)
     return false;
 }
 
+static bool isMAVLinkTelemetryHalfDuplex(void) {
+    return  (rxConfig()->receiverType == RX_TYPE_SERIAL && rxConfig()->serialrx_provider == SERIALRX_MAVLINK && tristateWithDefaultOffIsActive(rxConfig()->halfDuplex))
+            || telemetryConfig()->halfDuplex;
+}
+
 void handleMAVLinkTelemetry(timeUs_t currentTimeUs)
 {
     static bool incomingRequestServed;
@@ -1226,16 +1231,10 @@ void handleMAVLinkTelemetry(timeUs_t currentTimeUs)
 
     if ((currentTimeUs - lastMavlinkMessage) >= TELEMETRY_MAVLINK_DELAY && txbuff_free >= telemetryConfig()->mavlink.min_txbuff) {
         // Only process scheduled data if we didn't serve any incoming request this cycle
-        if (!incomingRequestServed ||
-            (
-                 (rxConfig()->receiverType == RX_TYPE_SERIAL) &&
-                 (rxConfig()->serialrx_provider == SERIALRX_MAVLINK) &&
-                 !tristateWithDefaultOnIsActive(rxConfig()->halfDuplex)
-            )
-        ) {
+        if (!incomingRequestServed || !isMAVLinkTelemetryHalfDuplex()) {
             processMAVLinkTelemetry(currentTimeUs);
+            lastMavlinkMessage = currentTimeUs;
         }
-        lastMavlinkMessage = currentTimeUs;
         incomingRequestServed = false;
     }
 }
