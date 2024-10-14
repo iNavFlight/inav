03df76265f460d5a97dbea7444ae13cac2c984f1
Peter Peiser <peter.peiser@gmail.com>
2024-10-14 21:11:16 +0200

    Updated to use the constrain function instead of the if statements

--- src/main/common/lulu.c
+++ src/main/common/lulu.c
@@ -17,13 +17,7 @@
 #include <fenv.h>
 
 void luluFilterInit(luluFilter_t *filter, int N) {
-    if (N > 15) {
-        N = 15;
-    }
-    if (N < 1) {
-        N = 1;
-    }
-    filter->N = N;
+    filter->N = constrain(N, 1, 15);
     filter->windowSize = filter->N * 2 + 1;
     filter->windowBufIndex = 0;
 
