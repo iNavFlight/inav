ae637e16891ec6abe04bde8c125fd54ea999d906
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2025-01-10 18:51:46 +0100

    Handle case where last runniner has no targets left to build

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -7,10 +7,16 @@ if(DEFINED CI_JOB_INDEX AND DEFINED CI_JOB_COUNT)
     math(EXPR per_job "(${count}+${CI_JOB_COUNT}-1)/${CI_JOB_COUNT}")
     message("-- ${per_job} targets per job")
     math(EXPR start "${CI_JOB_INDEX}*${per_job}")
-    list(SUBLIST targets ${start} ${per_job} ci_targets)
-    message("-- will build targets: ${ci_targets}")
-    add_custom_target(ci
-        ${CMAKE_COMMAND} -E true
-        DEPENDS ${ci_targets}
-)
+    if(${start} LESS ${count})
+        list(SUBLIST targets ${start} ${per_job} ci_targets)
+        message("-- will build targets: ${ci_targets}")
+        add_custom_target(ci
+            ${CMAKE_COMMAND} -E true
+            DEPENDS ${ci_targets}
+        )
+    else()
+        add_custom_target(ci
+            ${CMAKE_COMMAND} -E true
+        )
+    endif()
 endif()
