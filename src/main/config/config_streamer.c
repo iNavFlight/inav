206e958484eab8c5c82de34d37cb6b3f5a3645af
Marcelo Bezerra <>
2025-02-17 17:05:51 +0100

    Make config_streamer_stm32h7 less hard coded.

--- src/main/config/config_streamer.c
+++ src/main/config/config_streamer.c
@@ -15,80 +15,80 @@
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <string.h>
-#include "platform.h"
-#include "drivers/system.h"
-#include "config/config_streamer.h"
-#include "build/build_config.h"
-
-#if !defined(CONFIG_IN_FLASH)
-SLOW_RAM uint8_t eepromData[EEPROM_SIZE];
-#endif
-
-// Helper functions
-extern void config_streamer_impl_unlock(void);
-extern void config_streamer_impl_lock(void);
-extern int config_streamer_impl_write_word(config_streamer_t *c, config_streamer_buffer_align_type_t *buffer);
-
-void config_streamer_init(config_streamer_t *c)
-{
-    memset(c, 0, sizeof(*c));
-}
-
-void config_streamer_start(config_streamer_t *c, uintptr_t base, int size)
-{
-    // base must start at FLASH_PAGE_SIZE boundary when using embedded flash.
-    c->address = base;
-    c->size = size;
-    c->end = base + size;
-    if (!c->unlocked) {
-        config_streamer_impl_unlock();
-        c->unlocked = true;
-    }
-    c->err = 0;
-}
-
-int config_streamer_write(config_streamer_t *c, const uint8_t *p, uint32_t size)
-{
-    if ((c->address + size) > c->end) {
-        // trying to write past end of streamer
-        return -1;
-    }
-
-    for (const uint8_t *pat = p; pat != (uint8_t*)p + size; pat++) {
-        c->buffer.b[c->at++] = *pat;
-
-        if (c->at == sizeof(c->buffer)) {
-            c->err = config_streamer_impl_write_word(c, &c->buffer.w);
-            c->at = 0;
-        }
-    }
-    return c->err;
-}
-
-int config_streamer_status(config_streamer_t *c)
-{
-    return c->err;
-}
-
-int config_streamer_flush(config_streamer_t *c)
-{
-    if (c->at != 0) {
-        if ((c->address + c->at) > c->end) {
-            return -1;
-        }
-        memset(c->buffer.b + c->at, 0, sizeof(c->buffer) - c->at);
-        c->err = config_streamer_impl_write_word(c, &c->buffer.w);
-        c->at = 0;
-    }
-    return c-> err;
-}
-
-int config_streamer_finish(config_streamer_t *c)
-{
-    if (c->unlocked) {
-        config_streamer_impl_lock();
-        c->unlocked = false;
-    }
-    return c->err;
-}
+ #include <string.h>
+ #include "platform.h"
+ #include "drivers/system.h"
+ #include "config/config_streamer.h"
+ #include "build/build_config.h"
+ 
+ #if !defined(CONFIG_IN_FLASH)
+ SLOW_RAM uint8_t eepromData[EEPROM_SIZE];
+ #endif
+ 
+ // Helper functions
+ extern void config_streamer_impl_unlock(void);
+ extern void config_streamer_impl_lock(void);
+ extern int config_streamer_impl_write_word(config_streamer_t *c, config_streamer_buffer_align_type_t *buffer);
+ 
+ void config_streamer_init(config_streamer_t *c)
+ {
+     memset(c, 0, sizeof(*c));
+ }
+ 
+ void config_streamer_start(config_streamer_t *c, uintptr_t base, int size)
+ {
+     // base must start at FLASH_PAGE_SIZE boundary when using embedded flash.
+     c->address = base;
+     c->size = size;
+     c->end = base + size;
+     if (!c->unlocked) {
+         config_streamer_impl_unlock();
+         c->unlocked = true;
+     }
+     c->err = 0;
+ }
+ 
+ int config_streamer_write(config_streamer_t *c, const uint8_t *p, uint32_t size)
+ {
+     if ((c->address + size) > c->end) {
+         // trying to write past end of streamer
+         return -1;
+     }
+ 
+     for (const uint8_t *pat = p; pat != (uint8_t*)p + size; pat++) {
+         c->buffer.b[c->at++] = *pat;
+ 
+         if (c->at == sizeof(c->buffer)) {
+             c->err = config_streamer_impl_write_word(c, &c->buffer.w);
+             c->at = 0;
+         }
+     }
+     return c->err;
+ }
+ 
+ int config_streamer_status(config_streamer_t *c)
+ {
+     return c->err;
+ }
+ 
+ int config_streamer_flush(config_streamer_t *c)
+ {
+     if (c->at != 0) {
+         if ((c->address + c->at) > c->end) {
+             return -1;
+         }
+         memset(c->buffer.b + c->at, 0, sizeof(c->buffer) - c->at);
+         c->err = config_streamer_impl_write_word(c, &c->buffer.w);
+         c->at = 0;
+     }
+     return c-> err;
+ }
+ 
+ int config_streamer_finish(config_streamer_t *c)
+ {
+     if (c->unlocked) {
+         config_streamer_impl_lock();
+         c->unlocked = false;
+     }
+     return c->err;
+ }
--- src/main/config/config_streamer.h
+++ src/main/config/config_streamer.h
@@ -29,8 +29,8 @@
 #define CONFIG_STREAMER_BUFFER_SIZE M25P16_PAGESIZE // Must match flash device page size
 typedef uint32_t config_streamer_buffer_align_type_t;
 #elif defined(STM32H7)
-#define CONFIG_STREAMER_BUFFER_SIZE 32  // Flash word = 256-bits
-typedef uint64_t config_streamer_buffer_align_type_t;
+#define CONFIG_STREAMER_BUFFER_SIZE (FLASH_NB_32BITWORD_IN_FLASHWORD * 4) // Flash word = 256-bits
+typedef uint32_t config_streamer_buffer_align_type_t;
 #else
 #define CONFIG_STREAMER_BUFFER_SIZE 4
 typedef uint32_t config_streamer_buffer_align_type_t;
