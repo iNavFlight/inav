332bbefd5dea407a578dcd5146504c0fe8b055fc
Martijn <dixi83@users.noreply.github.com>
2025-10-21 19:33:33 +0200

    Update target configuration for FLYINGRCF4WINGMINI

--- src/main/target/FLYINGRCF4WINGMINI/target.h
+++ src/main/target/FLYINGRCF4WINGMINI/target.h
@@ -18,15 +18,11 @@
 #pragma once
 #define USE_TARGET_CONFIG
 
-#define TARGET_BOARD_IDENTIFIER "FRF4"
+#define TARGET_BOARD_IDENTIFIER "FRF4WM"
 #define USBD_PRODUCT_STRING  "FLYINGRCF4WINGMINI"
 
 #define LED0                    PA14  //Blue
 #define LED1                    PA13  //Green
-
-#define BEEPER                  PB9
-#define BEEPER_INVERTED
-#define BEEPER_PWM_FREQUENCY    2500
 
 // *************** SPI1 IMU & OSD *******************
 #define USE_SPI
@@ -37,30 +33,9 @@
 #define SPI1_MOSI_PIN   	    PA7
 
 #define USE_IMU_ICM42605
-#define IMU_ICM42605_ALIGN      CW270_DEG_FLIP
+#define IMU_ICM42605_ALIGN      CW90_DEG_FLIP
 #define ICM42605_SPI_BUS        BUS_SPI1
 #define ICM42605_CS_PIN         PC14
-
-#define USE_IMU_BMI270
-#define IMU_BMI270_ALIGN        CW270_DEG_FLIP
-#define BMI270_SPI_BUS          BUS_SPI1
-#define BMI270_CS_PIN           PC14
-
-#define USE_MAX7456
-#define MAX7456_SPI_BUS         BUS_SPI1
-#define MAX7456_CS_PIN          PB12
-
-// // *************** SPI2 Flash/SD Card  ****************
-// #define USE_SPI_DEVICE_2
-// #define SPI2_SCK_PIN            PB13
-// #define SPI2_MISO_PIN   	    PC2
-// #define SPI2_MOSI_PIN   	    PC3
-
-// #define USE_FLASHFS
-// #define USE_FLASH_M25P16
-// #define M25P16_SPI_BUS          BUS_SPI2
-// #define M25P16_CS_PIN           PC13
-// #define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
 
 // *************** I2C /Baro/Mag *********************
 #define USE_I2C
@@ -71,6 +46,9 @@
 #define USE_BARO
 #define BARO_I2C_BUS            BUS_I2C1
 #define USE_BARO_SPL06
+#define USE_BARO_BMP280
+#define USE_BARO_MS5611
+#define USE_BARO_DPS310
 
 #define USE_MAG
 #define MAG_I2C_BUS             BUS_I2C1
@@ -88,13 +66,9 @@
 #define UART1_TX_PIN            PA9
 #define UART1_RX_PIN            PA10
 
-// #define USE_UART2
-// #define UART2_TX_PIN            PA2
-// #define UART2_RX_PIN            PA3
-
-// #define USE_UART3
-// #define UART3_TX_PIN            PC10
-// #define UART3_RX_PIN            PC11
+#define USE_UART2
+#define UART2_TX_PIN            PA2
+#define UART2_RX_PIN            PA3
 
 #define USE_UART4
 #define UART4_TX_PIN            PA0
@@ -104,19 +78,15 @@
 #define UART5_TX_PIN            PC12
 #define UART5_RX_PIN            PD2
 
-// #define USE_UART6
-// #define UART6_TX_PIN            PC6
-// #define UART6_RX_PIN            PC7
-
 #define USE_SOFTSERIAL1
 #define SOFTSERIAL_1_TX_PIN      PA2
 #define SOFTSERIAL_1_RX_PIN      PA2
 
-#define SERIAL_PORT_COUNT       5
+#define SERIAL_PORT_COUNT       6
 
 #define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
 #define SERIALRX_PROVIDER       SERIALRX_CRSF
-#define SERIALRX_UART           SERIAL_PORT_USART2
+#define SERIALRX_UART           SERIAL_PORT_USART1
 
 // *************** ADC ***************************
 #define USE_ADC
@@ -124,16 +94,6 @@
 #define ADC1_DMA_STREAM             DMA2_Stream4
 #define ADC_CHANNEL_1_PIN           PC4
 #define VBAT_ADC_CHANNEL            ADC_CHN_1
-
-// *************** PINIO ***************************
-#define USE_PINIO
-#define USE_PINIOBOX
-#define PINIO1_PIN                  PA4
-#define PINIO2_PIN                  PB5
-
-// *************** LEDSTRIP ************************
-#define USE_LED_STRIP
-#define WS2811_PIN                  PB1
 
 // *************** others  ************************
 #define DEFAULT_FEATURES   (FEATURE_OSD | FEATURE_TELEMETRY | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TX_PROF_SEL | FEATURE_BLACKBOX)
@@ -145,10 +105,8 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define MAX_PWM_OUTPUT_PORTS    11
+#define MAX_PWM_OUTPUT_PORTS    7
 
 #define USE_SERIAL_4WAY_BLHELI_INTERFACE
 #define USE_DSHOT
-#define USE_DSHOT_DMAR
 #define USE_ESC_SENSOR
-
