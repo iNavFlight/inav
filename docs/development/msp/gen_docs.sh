e3aaf9de6caedbe2a53804d07b556f0d24e8a892
frogmane <7685285+xznhj8129@users.noreply.github.com>
2025-11-13 14:17:48 -0500

    mspdocs rev2

--- docs/development/msp/gen_docs.sh
+++ docs/development/msp/gen_docs.sh
@@ -1,16 +1,19 @@
+INAV_MAIN_PATH="../../../src/main"
+
 echo "###########"
 echo get_all_inav_enums_h.py
-python get_all_inav_enums_h.py
+python get_all_inav_enums_h.py --inav-root "$INAV_MAIN_PATH"
 
 echo "###########"
 echo "msp_messages.json checksum"
 actual="$(md5sum msp_messages.json | awk '{print $1}')"
 expected="$(awk '{print $1}' msp_messages.checksum)"
-
+echo "Hash:" $actual
 if [[ "$actual" != "$expected" ]]; then
   n="$(cat rev)"
   echo $((n + 1)) > rev
   echo "File changed, incrementing revision"
+  echo $actual > msp_messages.checksum
 fi
 
 echo "###########"
