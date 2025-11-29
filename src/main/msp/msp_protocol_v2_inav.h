7390501c15e1154ad6235ba1bf583dd7d8f4243e
frogmane <7685285+xznhj8129@users.noreply.github.com>
2025-11-29 11:14:03 -0500

    added msp set gvar

--- src/main/msp/msp_protocol_v2_inav.h
+++ src/main/msp/msp_protocol_v2_inav.h
@@ -122,5 +122,6 @@
 #define MSP2_INAV_SET_GEOZONE                   0x2211
 #define MSP2_INAV_GEOZONE_VERTEX                0x2212
 #define MSP2_INAV_SET_GEOZONE_VERTEX            0x2213
+#define MSP2_INAV_SET_GVAR                      0x2214
 
 #define MSP2_INAV_FULL_LOCAL_POSE               0x2220
