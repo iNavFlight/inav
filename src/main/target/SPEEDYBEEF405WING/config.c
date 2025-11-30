c9d521745d43e82037d6f2d30615edade3db65fb
genc4ik <50083271+genc4ik@users.noreply.github.com>
2025-11-30 14:54:37 +0300

    Update config.c

--- src/main/target/SPEEDYBEEF405WING/config.c
+++ src/main/target/SPEEDYBEEF405WING/config.c
@@ -1,15 +1,11 @@
 #include <stdbool.h>
 #include <stdint.h>
-#include <string.h>
 
 #include "platform.h"
 
 #include "fc/fc_msp_box.h"
 #include "io/serial.h"
 #include "io/piniobox.h"
-
-// Только САМЫЕ БАЗОВЫЕ includes
-#include "fc/config.h"
 
 void targetConfiguration(void)
 {
@@ -18,9 +14,4 @@ void targetConfiguration(void)
     serialConfigMutable()->portConfigs[findSerialPortIndexByIdentifier(SERIAL_PORT_USART6)].functionMask = FUNCTION_MSP;
 
     pinioBoxConfigMutable()->permanentId[0] = BOX_PERMANENT_ID_USER1;
-
-    // САМЫЕ БАЗОВЫЕ НАСТРОЙКИ
-    featureSet(FEATURE_MOTOR_STOP);
-    featureSet(FEATURE_PWM_OUTPUT_ENABLE);
-    featureSet(FEATURE_FW_AUTOTRIM);
 }
