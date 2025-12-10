31a5cbea8d74fd5ca8652e04b66a0fe83c9daab3
shota hayashi <miyafly7@gmail.com>
2025-12-11 01:10:23 +0900

    minor fix on comments

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -502,7 +502,7 @@ static float calculateTPAThtrottle(void)
         fpVector3_t vForward = { .v = { HeadVecEFFiltered.x, -HeadVecEFFiltered.y, -HeadVecEFFiltered.z } };
         float groundCos = vectorDotProduct(&vForward, &vDown);
         int16_t throttleAdjustment =  currentControlRateProfile->throttle.tpa_pitch_compensation * groundCos * 90.0f / (PI/2); //when 1deg pitch up, increase throttle by pitch(deg)_to_throttle. cos(89 deg)*90/(pi/2)=0.99995,cos(80 deg)*90/(pi/2)=9.9493,
-        throttleAdjustment= throttleAdjustment<0? throttleAdjustment/2:throttleAdjustment; //reduce throttle compensation when pitch down
+        throttleAdjustment= throttleAdjustment<0? throttleAdjustment/2:throttleAdjustment; //reduce throttle compensation when pitch up(when throttleAdjustment is negative which means trying to reduce "throttle")
         uint16_t throttleAdjusted = rcCommand[THROTTLE] + constrain(throttleAdjustment, -1000, 1000);
         tpaThrottle = pt1FilterApply(&fixedWingTpaFilter, constrain(throttleAdjusted, 1000, 2000));
     }
