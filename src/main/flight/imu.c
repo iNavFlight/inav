b7b4641f3e6c26d27400269988bcc29724939ebd
shota hayashi <miyafly7@gmail.com>
2026-02-06 22:47:04 +0900

    Update comments for airspeed selection logic in imuCalculateTurnRateacceleration function

--- src/main/flight/imu.c
+++ src/main/flight/imu.c
@@ -732,14 +732,14 @@ static void imuCalculateTurnRateacceleration(fpVector3_t *vEstcentrifugalAccelBF
 #ifdef USE_PITOT
     if (pitotGetValidForAirspeed())
     {
-        // second choice is pitot
+        // first choice is airspeed
 		currentspeed = getAirspeedEstimate();
         *acc_ignore_slope_multipiler = 4.0f;
     }
     else
 #endif
     if (isGPSTrustworthy()) {
-        //first speed choice is gps
+        // second choice is gps
         static bool lastGPSHeartbeat;
         static pt1Filter_t GPS3DspeedFilter;
         static float GPS3DspeedFiltered = 0.0f;
