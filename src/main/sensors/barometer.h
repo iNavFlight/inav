89838d2083f5958315833ec0874331a6861de110
Ray Morris <deepmagicbeginshere@gmail.com>
2026-04-12 15:04:23 -0500

    Fix BARO_CRSF enum value to preserve EEPROM compatibility
    BARO_FAKE has been value 12 since it was introduced. Inserting
    BARO_CRSF before it would renumber BARO_FAKE to 13, silently
    misinterpreting any stored baro_hardware config on upgrade.
    Move BARO_CRSF to 13 (after BARO_FAKE) to keep existing values stable.

--- src/main/sensors/barometer.h
+++ src/main/sensors/barometer.h
@@ -34,9 +34,9 @@ typedef enum {
     BARO_DPS310 = 9,
     BARO_B2SMPB = 10,
     BARO_MSP    = 11,
-    BARO_CRSF   = 12,
-    BARO_FAKE   = 13,
-    BARO_MAX    = BARO_FAKE
+    BARO_FAKE   = 12,
+    BARO_CRSF   = 13,
+    BARO_MAX    = BARO_CRSF
 } baroSensor_e;
 
 typedef struct baro_s {
