b59974fbeccb81d5515ef05068db61e50e0dd8a5
FlyingRC-Official <118458039+FlyingRC-Official@users.noreply.github.com>
2026-05-26 13:44:08 +0800

    Fix FlyingRC F4Wing Mini PR cleanup

--- src/main/target/FLYINGRCF4WINGMINI/target.h
+++ src/main/target/FLYINGRCF4WINGMINI/target.h
@@ -3,7 +3,7 @@
  *
  * INAV is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, o
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * INAV is distributed in the hope that it will be useful,
