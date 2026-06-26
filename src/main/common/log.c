e46efb632769c13f5017ee6a4bc7cec3cff6915f
FlyingRC-Official <118458039+FlyingRC-Official@users.noreply.github.com>
2026-06-26 09:05:22 +0800

    Fix SITL Mac warnings as errors

--- src/main/common/log.c
+++ src/main/common/log.c
@@ -205,9 +205,12 @@ void _logBufferHex(logTopic_e topic, unsigned level, const void *buffer, size_t 
 {
     // Print lines of up to maxBytes bytes. We need 5 characters per byte
     // 0xAB[space|\n]
-    const size_t charsPerByte = 5;
-    const size_t maxBytes = 8;
-    char buf[LOG_PREFIX_FORMATTED_SIZE + charsPerByte * maxBytes + 1]; // +1 for the null terminator
+    enum {
+        LOG_HEX_CHARS_PER_BYTE = 5,
+        LOG_HEX_MAX_BYTES = 8,
+        LOG_HEX_BUFFER_SIZE = LOG_PREFIX_FORMATTED_SIZE + LOG_HEX_CHARS_PER_BYTE * LOG_HEX_MAX_BYTES + 1,
+    };
+    char buf[LOG_HEX_BUFFER_SIZE]; // +1 for the null terminator
     size_t bufPos = LOG_PREFIX_FORMATTED_SIZE;
     const uint8_t *inputPtr = buffer;
 
@@ -219,7 +222,7 @@ void _logBufferHex(logTopic_e topic, unsigned level, const void *buffer, size_t 
 
     for (size_t ii = 0; ii < size; ii++) {
         tfp_sprintf(buf + bufPos, "0x%02x ", inputPtr[ii]);
-        bufPos += charsPerByte;
+        bufPos += LOG_HEX_CHARS_PER_BYTE;
         if (bufPos == sizeof(buf)-1) {
             buf[bufPos-1] = '\n';
             buf[bufPos] = '\0';
