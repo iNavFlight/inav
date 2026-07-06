9f54c8b6782387157d27f0836a58f716970273dd
Ray Morris <deepmagicbeginshere@gmail.com>
2026-07-06 16:05:10 -0500

    Add AXISFLYINGECOF4 target
    STM32F405 board with ICM42688P gyro (registered via the shared
    USE_IMU_ICM42605 driver, which detects ICM42605/ICM42686P/ICM42688P via
    WHO_AM_I), BMP280 and DPS310 baro auto-detect, MAX7456 OSD, M25P16 flash,
    4 motors, 2 servos, and a single PINIO pin.
    
    ADC instance corrected to ADC1 (INAV's F4 ADC driver only supports ADC1).
    Motor timer/DMA assignments (TIM8 CH1-4) match the already-shipping
    AIKONF4V3 target's identical configuration for the same pins.

---
+++ src/main/target/AXISFLYINGECOF4/target.h
@@ -0,0 +1,153 @@
+/*
+ * This file is part of INAV.
+ *
+ * INAV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INAV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with INAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TARGET_BOARD_IDENTIFIER "AXEC"
+#define USBD_PRODUCT_STRING     "AXISFLYINGECOF4"
+
+#define USE_TARGET_CONFIG
+
+// *************** LED, BEEPER ***************
+#define LED0                    PC5
+#define BEEPER                  PC3
+#define BEEPER_INVERTED
+
+// *************** IMU generic *****************
+#define USE_EXTI
+
+// *************** SPI1: Gyro & ACC  *******************
+#define USE_SPI
+#define USE_SPI_DEVICE_1
+#define SPI1_SCK_PIN            PA5
+#define SPI1_MISO_PIN           PA6
+#define SPI1_MOSI_PIN           PA7
+
+#define USE_IMU_ICM42605
+#define IMU_ICM42605_ALIGN      CW0_DEG
+#define ICM42605_SPI_BUS        BUS_SPI1
+#define ICM42605_CS_PIN         PA4
+#define ICM42605_EXTI_PIN       PC4
+
+// *************** SPI2: OSD *****************
+#define USE_SPI_DEVICE_2
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
+
+#define USE_MAX7456
+#define MAX7456_SPI_BUS         BUS_SPI2
+#define MAX7456_CS_PIN          PB12
+
+// *************** SPI3: Flash ***************
+#define USE_SPI_DEVICE_3
+#define SPI3_SCK_PIN            PB3
+#define SPI3_MISO_PIN           PB4
+#define SPI3_MOSI_PIN           PB5
+
+#define USE_FLASHFS
+#define USE_FLASH_M25P16
+#define M25P16_SPI_BUS          BUS_SPI3
+#define M25P16_CS_PIN           PB7
+#define ENABLE_BLACKBOX_LOGGING_ON_SPIFLASH_BY_DEFAULT
+
+// *************** I2C /Baro/Mag *********************
+#define USE_I2C
+#define USE_I2C_DEVICE_1
+#define I2C1_SCL                PB8
+#define I2C1_SDA                PB9
+
+#define USE_BARO
+#define BARO_I2C_BUS            BUS_I2C1
+#define USE_BARO_BMP280
+#define USE_BARO_DPS310
+
+#define USE_MAG
+#define MAG_I2C_BUS             BUS_I2C1
+#define USE_MAG_ALL
+
+// *************** UART *****************************
+#define USB_IO
+#define USE_VCP
+
+#define USE_UART1
+#define UART1_TX_PIN            PA9
+#define UART1_RX_PIN            PA10
+
+#define USE_UART2
+#define UART2_TX_PIN            NONE
+#define UART2_RX_PIN            PA3
+
+#define USE_UART3
+#define UART3_TX_PIN            PC10
+#define UART3_RX_PIN            PC11
+
+#define USE_UART4
+#define UART4_TX_PIN            PA0
+#define UART4_RX_PIN            PA1
+
+#define USE_UART5
+#define UART5_TX_PIN            PC12
+#define UART5_RX_PIN            PD2
+
+#define SERIAL_PORT_COUNT       6
+
+#define DEFAULT_RX_TYPE         RX_TYPE_SERIAL
+#define SERIALRX_PROVIDER       SERIALRX_SBUS
+#define SERIALRX_UART           SERIAL_PORT_USART2
+
+// *************** ADC *****************************
+#define USE_ADC
+#define ADC_INSTANCE                ADC1
+#define ADC1_DMA_STREAM              DMA2_Stream0
+#define ADC_CHANNEL_1_PIN           PC1
+#define ADC_CHANNEL_2_PIN           PC2
+#define VBAT_ADC_CHANNEL            ADC_CHN_1
+#define CURRENT_METER_ADC_CHANNEL   ADC_CHN_2
+
+#define DEFAULT_VOLTAGE_METER_SOURCE   VOLTAGE_METER_ADC
+#define DEFAULT_CURRENT_METER_SOURCE   CURRENT_METER_ADC
+
+// *************** LED2812 ************************
+#define USE_LED_STRIP
+#define WS2811_PIN              PB6
+
+// *************** CAMERA CONTROL *****************
+// CAMERA_CONTROL_PIN has no consuming driver prior to 10.0; becomes a PWM-capable PINIO pin from 10.0 forward.
+#define CAMERA_CONTROL_PIN      PB0
+
+// *************** PINIO ***************************
+#define USE_PINIO
+#define USE_PINIOBOX
+#define PINIO1_PIN              PC0
+#define PINIO1_FLAGS            PINIO_FLAGS_INVERTED
+
+// *************** Blackbox ************************
+#define DEFAULT_BLACKBOX_DEVICE        BLACKBOX_DEVICE_FLASH
+
+// ***************  OTHERS *************************
+#define DEFAULT_FEATURES        (FEATURE_TX_PROF_SEL | FEATURE_OSD | FEATURE_CURRENT_METER | FEATURE_VBAT | FEATURE_TELEMETRY | FEATURE_SOFTSERIAL)
+
+#define USE_SERIAL_4WAY_BLHELI_INTERFACE
+#define TARGET_IO_PORTA         0xffff
+#define TARGET_IO_PORTB         0xffff
+#define TARGET_IO_PORTC         0xffff
+#define TARGET_IO_PORTD         (BIT(2))
+
+#define MAX_PWM_OUTPUT_PORTS    6
+#define USE_DSHOT
+#define USE_ESC_SENSOR
