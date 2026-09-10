fdbfc571279312d36b590a6ee44c27700e96ffc7
Raphael Hunziker <raphael.hunziker@skysecdefence.ch>
2026-09-10 19:46:53 +0200

    msp: reject ESC 4-way passthrough while armed
    Fixes #11777

--- src/main/fc/fc_msp.c
+++ src/main/fc/fc_msp.c
@@ -226,7 +226,7 @@ static void mspSerialPassthroughFn(serialPort_t *serialPort)
     }
 }
 
-static void mspFcSetPassthroughCommand(sbuf_t *dst, sbuf_t *src, mspPostProcessFnPtr *mspPostProcessFn)
+static mspResult_e mspFcSetPassthroughCommand(sbuf_t *dst, sbuf_t *src, mspPostProcessFnPtr *mspPostProcessFn)
 {
     const unsigned int dataSize = sbufBytesRemaining(src);  /* Payload size in Bytes */
 
@@ -254,6 +254,11 @@ static void mspFcSetPassthroughCommand(sbuf_t *dst, sbuf_t *src, mspPostProcessF
          break;
 #ifdef USE_SERIAL_4WAY_BLHELI_INTERFACE
     case MSP_PASSTHROUGH_ESC_4WAY:
+        // entering the 4way interface stops the motor outputs, refuse while armed
+        if (ARMING_FLAG(ARMED)) {
+            return MSP_RESULT_ERROR;
+        }
+
         // get channel number
         // switch all motor lines HI
         // reply with the count of ESC found
@@ -267,6 +272,8 @@ static void mspFcSetPassthroughCommand(sbuf_t *dst, sbuf_t *src, mspPostProcessF
     default:
         sbufWriteU8(dst, 0);
     }
+
+    return MSP_RESULT_ACK;
 }
 
 static void mspRebootNormalFn(serialPort_t *serialPort)
@@ -5157,8 +5164,7 @@ mspResult_e mspFcProcessCommand(mspPacket_t *cmd, mspPacket_t *reply, mspPostPro
     } else if (mspFcProcessOutCommand(cmdMSP, dst, mspPostProcessFn)) {
         ret = MSP_RESULT_ACK;
     } else if (cmdMSP == MSP_SET_PASSTHROUGH) {
-        mspFcSetPassthroughCommand(dst, src, mspPostProcessFn);
-        ret = MSP_RESULT_ACK;
+        ret = mspFcSetPassthroughCommand(dst, src, mspPostProcessFn);
     } else if (cmdMSP == MSP_REBOOT) {
         if (!ARMING_FLAG(ARMED)) {
             ret = mspFcRebootCommand(src, mspPostProcessFn);
